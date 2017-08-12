8df912aecf2e990d69a34863385dab756ec99b65
Cata <30911915+Catamondium@users.noreply.github.com>
2017-08-12 16:36:29 +0100

    n/a

--- logic/flow-ctrl/iterative-20.c
+++ logic/flow-ctrl/iterative-20.c
@@ -5,7 +5,7 @@ int main () {
   
   for (n = 1; n <= 20; n = n +1) //initial value, do if true condition, iterate for repeat
   {
-    printf("%dth\n", n);
+    printf("%d\n", n);
   }
   return 0;
 }
