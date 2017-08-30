12c30678ddf3b19d1656425c03575b3b124e36e6
Cata <30911915+Catamondium@users.noreply.github.com>
2017-08-30 21:40:50 +0100

    Success!!

--- logic/flow-ctrl/recursion.c
+++ logic/flow-ctrl/recursion.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-int n;
-int factorial (n){
+main (){
+
+int i; //global input
+ scanf("%d", &i);
+int factorial (int n) { //local input
   
-  if ((n == 1) or (n == 0)) //test empty product, reduction resolve
+  if (n == 0) //test empty product, reduction resolve
   {
-    printf("1");
     return 1;//empty product
     }
   
   else { //continue n!
-    n * factorial(n-1); //multiply input by preceding number, recursively
-    printf("%d", int factorial(n));
+    n = n * factorial(n-1); //multiply input by preceding number, recursively
       }
   return n;
+}
+
+printf("%d\n", factorial(i)); //output
+return factorial(i);
 }
