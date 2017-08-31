8e5b2a784456264ee5ce71a3bf43711d256e4730
Cata <30911915+Catamondium@users.noreply.github.com>
2017-08-31 19:24:15 +0100

    Fixed, functions as expected

--- logic/flow-ctrl/iterative.c
+++ logic/flow-ctrl/iterative.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
-main (){
+main(){
   int INPUT;
-  scanf("%d", &INPUT); //input bodge
+  scanf("%d", &INPUT);
+  printf("%d\n", fibonacci(INPUT));
+}
+fibonacci(int i){
+  int first = 0;
+  int second = 1;
+  int result;
 
-  int fib(int i) {
-    int result;
-    if (i == 0) {return 0;}
-    if (i == 1) {return 1;}
-    else
-      {
-	for (int n = 0; n <= i; n++)
-	  {
-	    int first = 0;
-	    int second = 1;
-	    result = first + second; //summate next term
-	    first = second; //prepare next set
-	    second = result; //prepare next set
-	  }
-	return result;
-      }
+  if (i == 0)
+    {return 0;}
+  if (i == 1)
+    {return 1;}
+
+  else {
+    for (int n = 1; n < i; n++) {
+      result = first + second;
+      first = second;
+      second = result;
+    }
+    return result;
   }
-  
-  printf("%d\n", fib(INPUT)); //output
-  return fib(INPUT);
 }
