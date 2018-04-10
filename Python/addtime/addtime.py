642efbbadb0f66ee4826716b807cf48aaa3fef8c
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-10 23:24:26 +0100

    Improved input to take full time format(24hr), removed use of functions for simple calculations, leaving them commented out.

--- Python/addtime/addtime.py
+++ Python/addtime/addtime.py
@@ -1,23 +1,25 @@
-S_hrs = input("Start time hours:	")
-S_min = input("Start time minutes:	")
+
+instr = raw_input("Start time:	").split(':', 1)
 elapsed = input("Period(min):	")
-S_time = [S_hrs, S_min]
+S_time = list(map(int, instr))
 
-def get_min(t): # Get multiples of minutes
-	return t % 60
+# Undecided about including functions.
+#def get_min(t): # Get multiples of minutes
+	#return t % 60
 
-def get_hrs(t): # Get multiples of hours
-	return t // 60
+#def get_hrs(t): # Get multiples of hours
+	#return t // 60
 
-def hrs_min(t): # Hours to minutes
-	return t * 60
+#def hrs_min(t): # Hours to minutes
+	#return t * 60
 
 def calcE_time(t, St = []):
-	offset = hrs_min(St[0]) + St[1]
+	offset = St[0] * 60 + St[1]
 	tot = t + offset
-	return [get_hrs(tot), get_min(tot)]
+	return [tot // 60, tot % 60]
 
 E_time = calcE_time(elapsed, S_time)
 outstr = "\nStart time:	{0}:{1}	+{2}min\nEnd time:	{3}:{4}".format(S_time[0], S_time[1], elapsed, E_time[0], E_time[1])
 
 print(outstr)
+
