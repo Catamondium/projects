6e4a0406dd06c455317c2dc8d1f8e9d9d32cc82d
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-12 00:22:21 +0100

    Added entirely inline option on first prompt, began working out for commandline arguments.

--- Python/addtime/addtime.py
+++ Python/addtime/addtime.py
@@ -1,16 +1,17 @@
-instr = raw_input("Start time:	").split(':', 1) # Collect [hrs, min] as string
-elapsed = input("Period(min):	")
-S_time = [int(x) for x in instr] # Cast to int
+import sys
 
-# Undecided about including functions.
-#def get_min(t): # Get multiples of minutes
-	#return t % 60
+Istr = raw_input("Start time:	") # Collect [hrs, min, [elapsed]] as string
 
-#def get_hrs(t): # Get multiples of hours
-	#return t // 60
+Istr = Istr.replace(' ', ':').split(':', 2) # Split into list
+Istr = [int(x) for x in Istr]
 
-#def hrs_min(t): # Hours to minutes
-	#return t * 60
+if len(Istr) == 2: # If e.g 3:30
+	S_time = Istr
+	elapsed = input("Period(min):	")
+
+else: # If e.g 3:30 60
+	S_time = [Istr[0], Istr[1]]
+	elapsed = Istr[2]
 
 def calcE_time(t, St = []):
 	offset = St[0] * 60 + St[1]
@@ -18,6 +19,10 @@ def calcE_time(t, St = []):
 	return [tot // 60, tot % 60]
 
 E_time = calcE_time(elapsed, S_time)
-outstr = "\nStart time:	{0}:{1}	+{2}min\nEnd time:	{3}:{4}".format(S_time[0], S_time[1], elapsed, E_time[0], E_time[1])
+Ostr = "\nStart time:	{0}:{1}	+{2}min\nEnd time:	{3}:{4}".format(S_time[0], S_time[1], elapsed, E_time[0], E_time[1])
 
-print(outstr)
+print(Ostr)
+print(str(sys.argv))
+
+# Commandline output test:	./addtime.pyc 3:30 111
+#	['./addtime.pyc', '3:30', '111']
