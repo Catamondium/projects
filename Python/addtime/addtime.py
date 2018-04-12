894c46c825db0c531c7f78947ce6f3909b33a4d1
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-13 00:05:43 +0100

    Linted with new ide, apparent input & raw_input functions aren't Python3?? Left as is due to errors by the interpereter. Removed redundant .pyc file.

--- Python/addtime/addtime.py
+++ Python/addtime/addtime.py
@@ -1,28 +1,30 @@
 import sys
 
 if len(sys.argv) == 1:
-	Istr = raw_input("Start time:	")
-	Istr = Istr.replace(' ', ':').split(':', 2)
+        Istr = raw_input("Start time:	")
+        Istr = Istr.replace(' ', ':').split(':', 2)
 else:
-	Istr = sys.argv[1].split(':')
-	if len(sys.argv) >= 3:
-		Istr[len(Istr):] = [sys.argv[2]]
+        Istr = sys.argv[1].split(':')
+        if len(sys.argv) >= 3:
+            Istr[len(Istr):] = [sys.argv[2]]
 
 Istr = [int(x) for x in Istr]
-if len(Istr) == 2: # Supplement input
-	S_time = Istr
-	elapsed = input("Period(min):	")
+if len(Istr) == 2:  # Supplement input
+    S_time = Istr
+    elapse = input("Period(min):	")
 
-else: # Use existing
-	S_time = Istr[:2] # Take 1st 2 elements
-	elapsed = Istr[2]
+else:  # Use existing
+        S_time = Istr[:2]  # Take 1st 2 elements
+        elapse = Istr[2]
 
-def calcE_time(t, St = []):
-	offset = St[0] * 60 + St[1]
-	tot = t + offset
-	return [tot // 60, tot % 60]
 
-E_time = calcE_time(elapsed, S_time)
-Ostr = "Start time:	{0}:{1}	+{2}min\nEnd time:	{3}:{4}".format(S_time[0], S_time[1], elapsed, E_time[0], E_time[1])
+def calcE_time(t, St=[]):
+        offset = St[0] * 60 + St[1]
+        tot = t + offset
+        return [tot // 60, tot % 60]
 
-print(Ostr)
+E_time = calcE_time(elapse, S_time)
+Ostr_1 = "Start time:	{0}:{1}	+{2}min".format(S_time[0], S_time[1], elapse)
+Ostr_2 = "\nEnd time:	{0}:{1}".format(E_time[0], E_time[1])
+
+print((Ostr_1 + Ostr_2))
