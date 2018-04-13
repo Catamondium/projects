b8fae377301730bb0ce51e78b538019c51f2aae1
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-13 10:19:44 +0100

    Linting

--- Python/addtime/addtime.py
+++ Python/addtime/addtime.py
@@ -3,27 +3,27 @@
 import sys
 
 if len(sys.argv) == 1:
-        Istr = input("Start time:	")
-        Istr = Istr.replace(' ', ':').split(':', 2)
+	Istr = input("Start time:	")
+	Istr = Istr.replace(' ', ':').split(':', 2)
 else:
-        Istr = sys.argv[1].split(':')
-        if len(sys.argv) >= 3:
-            Istr[len(Istr):] = [sys.argv[2]]
+	Istr = sys.argv[1].split(':')
+	if len(sys.argv) >= 3:
+		Istr[len(Istr):] = [sys.argv[2]]
 
 Istr = [int(x) for x in Istr]
 if len(Istr) == 2:  # Supplement input
-    Stime = Istr
-    elapse = eval(input("Period(min):	"))
+	Stime = Istr
+	elapse = eval(input("Period(min):	"))
 
 else:  # Use existing
-        Stime = Istr[:2]  # Take 1st 2 elements
-        elapse = Istr[2]
+	Stime = Istr[:2]  # Take 1st 2 elements
+	elapse = Istr[2]
 
 
 def calcEtime(t, St=[]):
-        offset = St[0] * 60 + St[1]
-        tot = t + offset
-        return [tot // 60, tot % 60]
+	offset = St[0] * 60 + St[1]
+	tot = t + offset
+	return [tot // 60, tot % 60]
 
 Etime = calcEtime(elapse, Stime)
 Ostr_1 = "Start time:	{0:02d}:{1:02d}".format(Stime[0], Stime[1])
