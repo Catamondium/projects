0bddd962c8dc26bf6d1aa6706c4cbe53a18d36e3
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-15 00:59:38 +0100

    Attempted syntax fixes and output formatting

--- C/addtime/addtime.c
+++ C/addtime/addtime.c
@@ -1,16 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+void calcEtime(int St[2], int t, float Et[2]) {
+	int offset = St[0] * 60 + St[1];
+	int tot = offset + t;
+	Et[0] = floor(tot / 60);
+	Et[1] = tot % 60;
+	return;
+	} 
+		
 int main(int argc, char *argv[]) {
 	int elapse = 90;
 	int Stime[2] = {1, 30};
 	
-	int Etime[2];
+	float Etime[2];
 	calcEtime(Stime, elapse, Etime);
 	
-	printf(Etime);
+	printf("End time\t%02f:%02f\n", Etime[0], Etime[1]);
 	}
-
-	void calcEtime(int St[2], int t, int Et[2]) {
-		int offset = St[0] * 60 + St[1];
-		int tot = offset + t;
-		Et = {floor(tot / 60), tot % 60};
-		return;
-		} 
