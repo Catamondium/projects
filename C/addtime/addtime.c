2dfcd06e1e8caba75afc1d93d8d37338c7d811a0
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-15 01:32:30 +0100

    This is gonna be a rough one, messed up formatting massively with sprintf(), primary calcEtime() is functioning though.

--- C/addtime/addtime.c
+++ C/addtime/addtime.c
@@ -11,10 +11,15 @@ void calcEtime(int St[2], int t, float Et[2]) {
 		
 int main(int argc, char *argv[]) {
 	int elapse = 90;
-	int Stime[2] = {1, 30};
+	int Stime[2] = {1, 30}; // Expect 3:00 out
 	
 	float Etime[2];
 	calcEtime(Stime, elapse, Etime);
 	
-	printf("End time\t%02f:%02f\n", Etime[0], Etime[1]);
+	char Ostr_1[15];
+	sprintf(Ostr_1, "Start time: %02.0f:%02.0f", Stime[0], Stime[1]);
+	char Ostr_2[15];
+	sprintf(Ostr_2, "End time: %02.0f:%02.0f\n", Etime[0], Etime[1]);
+	
+	printf("%s %+i\n%s", Ostr_1, elapse, Ostr_2);
 	}
