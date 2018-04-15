e9624f795c2610ae6796c9ea982eeda641216a96
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-15 11:05:44 +0100

    Command line arguments work for elapse variable, not currently working for Stime[].

--- C/addtime/addtime.c
+++ C/addtime/addtime.c
@@ -1,26 +1,36 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void calcEtime(int St[2], int t, float Et[2]) {
+void calcEtime(int St[2], int t, int Et[2]) {
 	int offset = St[0] * 60 + St[1];
 	int tot = offset + t;
-	Et[0] = floor(tot / 60);
-	Et[1] = tot % 60;
+	Et[0] = (int) (floor(tot / 60));
+	Et[1] = (int) (tot % 60);
 	return;
 	} 
 		
 int main(int argc, char *argv[]) {
-	int elapse = 90;
-	int Stime[2] = {1, 30}; // Expect 3:00 out
+	int elapse, Stime[2], Etime[2];
+	const char delimiters[] = " :";
 	
-	float Etime[2];
+	if(argc < 3) {
+		return 1;
+	} else {
+		elapse = atoi(argv[2]);
+		printf("%i\n", elapse);
+		
+		// TODO: split argv[1] by ':'
+	}
+	
+	elapse = 90;
+	Stime[0] = 1;
+	Stime[1] = 30; // Expect 3:00 out
+		
+	Etime[2];
 	calcEtime(Stime, elapse, Etime);
 	
-	//char Ostr_1[15];
-	printf("Start time:\t%02.0f:%02.0f", Stime[0], Stime[1]);
-	printf("\t%+i\n", elapse);
-	//char Ostr_2[15];
-	printf("End time:\t%02.0f:%02.0f\n", Etime[0], Etime[1]);
-	
-	//printf("%s %+i\n%s", Ostr_1, elapse, Ostr_2);
+	printf("Start time:\t%02i:%02i", Stime[0], Stime[1]);
+	printf("\t%+d\n", elapse);
+	printf("End time:\t%02i:%02i\n", Etime[0], Etime[1]);
 	}
