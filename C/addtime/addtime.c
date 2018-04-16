c1a2d290bbae15f7ba13b4c1f589525c3a422a61
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-16 19:04:41 +0100

    Linting C code.

--- C/addtime/addtime.c
+++ C/addtime/addtime.c
@@ -3,38 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-void calcEtime(int St[2], int t, int Et[2]) {
-	int offset = St[0] * 60 + St[1];
-	int tot = offset + t;
-	Et[0] = (int) (floor(tot / 60));
-	Et[1] = (int) (tot % 60);
-	return;
-	} 
-		
-int main(int argc, char *argv[]) {
-	int Stime[2], Etime[2];
-	signed int elapse;
-	
-	if(argc < 3) {
-		printf("Error\tmm:hh mins expected.");
-		return 1;
-	} else {
-		elapse = atoi(argv[2]);
-		// Split and collect start time
-		const char delimiters[] = " :";
-		char in[60];
-		strcpy(in, argv[1]);
-		char * hrs;
-		char * mins;
-		hrs = strtok(in, delimiters);
-		mins = strtok(NULL, delimiters);
-		Stime[0] = atoi(hrs);
-		Stime[1] = atoi(mins);
-	}
-		
-	calcEtime(Stime, elapse, Etime);
-	
-	printf("Start time:\t%02i:%02i", Stime[0], Stime[1]);
-	printf("\t%+d\n", elapse);
-	printf("End time:\t%02i:%02i\n", Etime[0], Etime[1]);
-	}
+void calcEtime(int St[2], int t, int Et[2])
+{
+    int offset = St[0] * 60 + St[1];
+    int tot = offset + t;
+    Et[0] = (int) (floor(tot / 60));
+    Et[1] = (int) (tot % 60);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    int Stime[2], Etime[2];
+    signed int elapse;
+
+    if(argc < 3)
+    {
+        printf("Error\tmm:hh mins expected.");
+        return 1;
+    }
+    else
+    {
+        elapse = atoi(argv[2]);
+        // Split and collect start time
+        const char delimiters[] = " :";
+        char in[60];
+        strcpy(in, argv[1]);
+        char * hrs;
+        char * mins;
+        hrs = strtok(in, delimiters);
+        mins = strtok(NULL, delimiters);
+        Stime[0] = atoi(hrs);
+        Stime[1] = atoi(mins);
+    }
+
+    calcEtime(Stime, elapse, Etime);
+
+    printf("Start time:\t%02i:%02i", Stime[0], Stime[1]);
+    printf("\t%+d\n", elapse);
+    printf("End time:\t%02i:%02i\n", Etime[0], Etime[1]);
+}
--- C/fibonacci/fibo.c
+++ C/fibonacci/fibo.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(int n) {
-  if (n == 0) {
-	  return 0;
-	  }
-  if (n == 1) {
-	  return 1;
-	  } else {
-		  int first = 0;
-		  int second = 1;
-		  int result;
-		  for (int i = 1; i < n; i++) {
-			  result = first + second;
-			  first = second;
-			  second = result;
-			  }
-			  return result;
- }
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int first = 0;
+        int second = 1;
+        int result;
+        for (int i = 1; i < n; i++)
+        {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
 }
 
-int main(int argc, char *argv[]) {
-	if (argc == 1) {
-		printf("Error, integer argument required.\n");
-	} else {
-		int n = atoi(argv[1]);
-		printf("%d\n", fibonacci(n));
- }
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("Error, integer argument required.\n");
+    }
+    else
+    {
+        int n = atoi(argv[1]);
+        printf("%d\n", fibonacci(n));
+    }
 }
