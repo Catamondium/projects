156eb50e90dd30842b19a90f5374b5f9c38f093e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-16 17:59:39 +0100

    Restored fibonacci program, refactoring for command line input.

---
+++ C/fibonacci/fibo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n) {
+  if (n == 0) {
+	  return 0;
+	  }
+  if (n == 1) {
+	  return 1;
+	  } else {
+		  int first = 0;
+		  int second = 1;
+		  int result;
+		  for (int i = 1; i < n; i++) {
+			  result = first + second;
+			  first = second;
+			  second = result;
+			  }
+			  return result;
+ }
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		printf("Error, integer argument required.\n");
+	} else {
+		int n = atoi(argv[1]);
+		printf("%d\n", fibonacci(n));
+ }
+}
