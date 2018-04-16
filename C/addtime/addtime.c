3906230cda970ea8d0fb8eda0374bb0de5392989
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-04-16 17:35:15 +0100

    Successfully implemented command line input, cleaned up debugging statements.

--- C/addtime/addtime.c
+++ C/addtime/addtime.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void calcEtime(int St[2], int t, int Et[2]) {
 	int offset = St[0] * 60 + St[1];
@@ -13,23 +14,24 @@ void calcEtime(int St[2], int t, int Et[2]) {
 int main(int argc, char *argv[]) {
 	int Stime[2], Etime[2];
 	signed int elapse;
-	const char delimiters[] = " :";
 	
 	if(argc < 3) {
 		printf("Error\tmm:hh mins expected.");
 		return 1;
 	} else {
 		elapse = atoi(argv[2]);
-		printf("%i\n", elapse);
-		
-		// TODO: split argv[1] by ':'
+		// Split and collect start time
+		const char delimiters[] = " :";
+		char in[60];
+		strcpy(in, argv[1]);
+		char * hrs;
+		char * mins;
+		hrs = strtok(in, delimiters);
+		mins = strtok(NULL, delimiters);
+		Stime[0] = atoi(hrs);
+		Stime[1] = atoi(mins);
 	}
-	
-	elapse = 90;
-	Stime[0] = 1;
-	Stime[1] = 30; // Expect 3:00 out
 		
-	Etime[2];
 	calcEtime(Stime, elapse, Etime);
 	
 	printf("Start time:\t%02i:%02i", Stime[0], Stime[1]);
