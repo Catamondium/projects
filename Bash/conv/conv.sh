3ab0cca5bd7467101913d4d60b5d7b23592a36f9
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-05-12 19:41:11 +0100

    Added Bash converstion program for converting arbitrarily formatted "data.txt" to tsv formatted "result.tsv".

---
+++ Bash/conv/conv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#GNU bash, version 4.3.48(1)-release (x86_64-pc-linux-gnu)
+
+subject="./data.txt"
+result="./result.tsv"
+
+chunk_width=3 # 3 newlines for every row of data
+length=`wc -l < $subject` # Get number of newlines
+let "length--" # Offset for final newline
+let num=length/chunk_width
+
+# Create .tsv
+echo -e "name\tnum\tletter" > $result # Write header
+for i in `seq 1 $num`; do # Read
+	let v=i
+	name=(`sed "${v}q;d" $subject`) # Every 1st line
+	let "v++"
+	num=(`sed "${v}q;d" $subject`) # Every 2nd line
+	let "v++"
+	letter=(`sed "${v}q;d" $subject`) # Every 3rd line
+	echo -e "$name\t$num\t$letter" >> $result #Write body
+done
+
