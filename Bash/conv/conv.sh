55bf70ee489e30e3dbd057da9fdf547b55885e0a
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-05-12 21:25:33 +0100

    Minor clarity adjustments

--- Bash/conv/conv.sh
+++ Bash/conv/conv.sh
@@ -4,7 +4,7 @@
 subject="./data.txt"
 result="./result.tsv"
 
-chunk_width=3 # 3 newlines for every row of data
+chunk_width=3 # 3 lines for every row of data
 length=`wc -l < $subject` # Get number of newlines
 let "length--" # Offset for final newline
 
@@ -21,6 +21,6 @@ for i in `seq 1 $chunk_width $length`; do # Read
 	let "v=$i+2"
 	letter=`sed "${v}q;d" $subject` # Every 3rd line
 
-	echo -e "$name\t$num\t$letter" >> $result #Write body row
+	echo -e "$name\t$num\t$letter" >> $result #bWrite body row
 done
 
