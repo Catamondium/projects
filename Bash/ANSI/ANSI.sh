3325d1af8c406a09dc3d32aa650b77ee629f7678
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-05-13 15:14:32 +0100

    Adjusted for underline exception and removed responsibility from blink entry

--- Bash/ANSI/ANSI.sh
+++ Bash/ANSI/ANSI.sh
@@ -16,10 +16,10 @@ echo -e "\tReverse video\t" >> $1
 tput smul >> $1
 echo -ne "\tUnderlined\t" >> $1
 tput rmul >> $1
-echo -ne "\n"
+echo >> $1
 
 tput blink >> $1
-echo -e "\n\tBlinking\t" >> $1
+echo -e "\tBlinking\t" >> $1
 
 
 tput bold >> $1
@@ -27,10 +27,10 @@ echo -e "\tBold\t" >> $1
 
 tput sgr0 >> $1 # Reset attributes
 echo -e "\tReset\t" >> $1
-for C in {0..256}; do # Print colour codes
+for C in {0..255}; do # Print colour codes
 	tput setab $C >> $1
 	echo -e "\t$C\t" >> $1
 done
+
 tput sgr0 # Reset attributes
 echo >> $1 # Final newline
-cat $1
