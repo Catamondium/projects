1ba4ed657dcbb665ee8b83c40e215e4b40035212
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-06-14 19:15:49 +0100

    Refactored scripts to use arithmetic expansion instead of the 'let' command

--- Bash/conv/conv.sh
+++ Bash/conv/conv.sh
@@ -6,19 +6,19 @@ result="./result.tsv"
 
 chunk_width=3 # 3 lines for every row of data
 length=`wc -l < $subject` # Get number of newlines
-let "length--" # Offset for final newline
+((length--)) # Offset for final newline
 
 # Create .tsv
 echo -e "name\tnum\tletter" > $result # Write header
 
 for i in `seq 1 $chunk_width $length`; do # Read
-	let "v=$i"
+	((v=$i))
 	name=`sed "${v}q;d" $subject` # Every 1st line
 
-	let "v=$i+1"
+	((v=$i+1))
 	num=`sed "${v}q;d" $subject` # Every 2nd line
 
-	let "v=$i+2"
+	((v=$i+2))
 	letter=`sed "${v}q;d" $subject` # Every 3rd line
 
 	echo -e "$name\t$num\t$letter" >> $result # Write body row
