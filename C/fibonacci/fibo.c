1d3103d1e109bbaff90f279ca6baadcff1405990
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-07-01 12:40:08 +0100

    Now blocking overflow limit in fibonacci results and used typedef to simplify declarations.

--- C/fibonacci/fibo.c
+++ C/fibonacci/fibo.c
@@ -1,17 +1,18 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <stdio.h>
+typedef long long int big_number;
 
-unsigned long long int fibonacci(int n) {
+big_number fibonacci(int n) {
     if (n == 0) {
         return 0;
     }
     if (n == 1) {
         return 1;
     } else {
-        unsigned long long int first = 0;
-        unsigned long long int second = 1;
-        unsigned long long int result;
+        big_number first = 0;
+        big_number second = 1;
+        big_number result;
         for (int i = 1; i < n; i++) {
             result = first + second;
             first = second;
@@ -28,9 +29,17 @@ int main(int argc, char *argv[]) {
     } else {
         int n = atoi(argv[1]);
         for(int i = 0; i < n; ++i) {
-            printf("%'lld\t", fibonacci(i));
-            if((i + 1) % 7 == 0 && i != 0) {
-                printf("\n");
+            big_number z = fibonacci(i);
+
+            if (z < 0) {
+                printf("Limit at:\t%dth", i-1);
+                break;
+
+            } else {
+                printf("%lld\t", z);
+                if((i + 1) % 7 == 0 && i != 0) {
+                    printf("\n");
+                }
             }
         }
         printf("\n");
