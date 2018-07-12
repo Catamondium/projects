81cdeb510185b580342cfd3aeb8bb395b1219413
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-07-12 23:12:33 +0100

    The CLisp version of addtime is now faithful to the C version, save tabbing out output strings.

--- Lisp/addtime.lisp
+++ Lisp/addtime.lisp
@@ -1,5 +1,16 @@
 (defstruct time_c hrs mins)
 
+(defun parse-time (str)
+  (setq i 0)
+  (loop
+     (setq c (char str i))
+     (char= c #\:) ; Split by ':' delimiter
+     (return)
+     (+ i 1))
+  (make-time_c
+   :hrs (parse-integer (subseq str 0 (- 1 i)))
+   :mins (parse-integer (subseq str (+ i 2)))))
+
 (defun calctime (S to_elapse)
   (setq offset (+ (* (time_c-hrs S) 60) (time_c-mins S)))
   (setq total (+ offset to_elapse))
@@ -7,10 +18,11 @@
    :hrs (floor (/ total 60))
    :mins (mod total 60)))
 
-(setq start (make-time_c :hrs 1 :mins 30))
-(setq elapse -60)
+; Main
+(setq start (parse-time (pop *args*)))
+(setq elapse (parse-integer (pop *args*)))
 (setq end (calctime start elapse))
 
-(format t "start: ~2,'0D:~2,'0D ~D~%" (time_c-hrs start)
+(format t "start: ~2,'0D:~2,'0D ~@D~%" (time_c-hrs start)
 	(time_c-mins start) elapse)
 (format t "End: ~2,'0D:~2,'0D~%" (time_c-hrs end) (time_c-mins end))
