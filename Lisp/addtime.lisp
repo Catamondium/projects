28d3889c0b230faca6a0d7e58bc6bc2cb844c03d
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-07-18 03:01:35 +0100

    now using do loop in place of loop.

--- Lisp/addtime.lisp
+++ Lisp/addtime.lisp
@@ -1,15 +1,12 @@
 (defstruct time_c hrs mins)
 
 (defun parse-time (str)
-  (setq i 0)
-  (loop
-     (setq c (char str i))
-     (char= c #\:) ; Split by ':' delimiter
-     (return)
-     (+ i 1))
+  (setq index (do ((i 0 (+ i 1)))
+		  ((char= #\: (char str i) ; Split by ':' delimiter
+		(return i) 0))))
   (make-time_c
-   :hrs (parse-integer (subseq str 0 (- 1 i)))
-   :mins (parse-integer (subseq str (+ i 2)))))
+   :hrs (parse-integer (subseq str 0 (- 1 index)))
+   :mins (parse-integer (subseq str (+ index 2)))))
 
 (defun calctime (S to_elapse)
   (setq offset (+ (* (time_c-hrs S) 60) (time_c-mins S)))
@@ -25,4 +22,4 @@
 
 (format t "start: ~2,'0D:~2,'0D ~@D~%" (time_c-hrs start)
 	(time_c-mins start) elapse)
-(format t "End: ~2,'0D:~2,'0D~%" (time_c-hrs end) (time_c-mins end))
+(format t "End ,'0D:~2,'0D~%" (time_c-hrs end) (time_c-mins end))
