a49a3e8402a9fee1411688e3a0e6baac24f1d0bd
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-08-05 00:55:05 +0100

    Refactored OO version of ngons.

--- Python/ngons/ngons.py
+++ Python/ngons/ngons.py
@@ -4,46 +4,36 @@ from turtle import *
 import sys
 import math
 
-def ty(n, R):  # y component to beginning vertex of ngonR
-	length = getlength(n, R)
-	return - (R**2 - (.5 * length)**2)**.5
-def tx(n, R):  # x component to beginning vertex of ngonR
-    return .5 * getlength(n, R)
+class ngonR:
+    def __init__(self, n, R, x, y):
+        self.n = n
+        self.R = R
+        self.y = y # True centre
+        self.x = x # True centre
 
-def getlength(n, R):  # Find length of regular ngonR
-	return R * 2 * math.sin(math.pi / n)
+        self.len = R * 2 * math.sin(math.pi / n)
+        self.oy = - (R**2 - (.5 * self.len)**2)**.5 + y # Begin vertex
+        self.ox = .5 * self.len + x # Begin vertex
 
-def ngon(n, R, t):  # Generate regular ngonR, with Turtle t
-	length = getlength(n, R)
-	for i in range(0, n):
-		t.left(180 - ((1 - (2 / n)) * 180))  # t rotates by exterior angles
-		t.forward(length)
-		title('ngon: {}'.format(n))
-# Settings
-terry = Turtle()
-bgcolor('black')
-terry.color('red', 'purple')
-terry.shape('turtle')
-title(sys.argv[0])
-# Draw shapes
-r = 200
-last_n = 0
-for n in range(3, 8):
-	terry.pu()
-	terry.setpos(tx(n, r), ty(n, r))
-	terry.pd()
-	terry.begin_fill()
-	ngon(n, r, terry)
-	terry.end_fill()
-	last_n = n
-# Draw measured circumradius
-str_rad = 'R = {}'.format(round(terry.distance(0, 0)))
-terry.color('black')
-terry.setpos(tx(last_n, r) / 2, ty(last_n, r) / 2)
-terry.write(str_rad)
-terry.home()
-terry.color('red')
-#terry.ht()
+    def draw(self, col_L, col_F):
+        t = Turtle()
+        t.color(col_L, col_F)
+        t.pu()
+        t.setpos(self.ox, self.oy)
+        t.begin_fill()
+        t.pd()
+        for i in range(0, self.n):
+            t.left(180 - ((1 - (2 / self.n)) * 180))
+            t.forward(self.len)
+        t.end_fill()
+        t.setpos(self.x, self.y)
+        t.pu()
+
+# Main
+bgcolor("black")
+for n in range(3, 10):
+        shape = ngonR(n, 200, 0, 0)
+        shape.draw("red", "purple")
 # Screenshot
 canvas = getcanvas()
 canvas.postscript(file='out.ps')
