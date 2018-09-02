87a8ad68739d608b079cdef51e781f426fe935e3
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-09-02 22:10:44 +0100

    Added basic client-server.

---
+++ Python/socket/client.py
@@ -0,0 +1,8 @@
+import socket
+
+clidata = socket.gethostname(), 5007
+
+with socket.socket() as client:
+    client.connect(clidata)
+    msg = client.recv(1024)
+print("Recieved:\t%s" % msg.decode("ascii"), end='')
