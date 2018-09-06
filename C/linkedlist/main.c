f05f047360a6cbf797fb0e1c2a43d91993957fa3
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-09-06 19:38:59 +0100

    Integrated test run into Makefile, added assertions.

--- C/linkedlist/main.c
+++ C/linkedlist/main.c
@@ -12,17 +12,22 @@ int main() {
 		insert(&list, i, i);
 	}
 
+	assert(length(list)==10);
 	printf("Append 10:\t%d items\n", length(list));
 
+	insert(&list, 5, 9999);
+
+	printf("Insert@5:\t%d items\n", length(list));
+
+
 	printf("\nGetting%s", BOUNDARY);
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < length(list); i++) {
 		printf("%d:\t%d\n", i, get(list, i));
 	}
 
-	printf("\nPopping%s", BOUNDARY);
-	for(int i = 0; i < 10; i++) { // Good place to test length
-		int y = pop(&list);
-		printf("%d:\t%d\n", i, y);
+	printf("\nPopping%s", BOUNDARY); // only 5 popped?
+	for(int i = 0; length(list) != 0; i++) { // changing alongside us?
+		printf("%d:\t%d\n", i, pop(&list));
 	}
 
 	printf("\nemptyList:\t%d items\n", length(list));
