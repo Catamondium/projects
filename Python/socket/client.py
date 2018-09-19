bbbce7ce73f8c39b2b37ee3310eb994993dfe11c
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-09-19 22:07:30 +0100

    Specified msg length.

--- Python/socket/client.py
+++ Python/socket/client.py
@@ -4,5 +4,5 @@ clidata = socket.gethostname(), 5007
 
 with socket.socket() as client:
     client.connect(clidata)
-    msg = client.recv(1024)
+    msg = client.recv(24)
 print("Recieved:\t%s" % msg.decode(), end='')
