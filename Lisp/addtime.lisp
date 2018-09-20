51376f7043f2d80c94ce5c526ba5c41dc030d174
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-09-20 23:17:40 +0100

    Implemented time_c as a class, using print-object as equivalent to python's __repr__ method.

--- Lisp/addtime.lisp
+++ Lisp/addtime.lisp
@@ -1,15 +1,24 @@
-(defstruct time_c hrs mins)
+(defclass time_c () 
+  ((hrs :accessor time-hrs
+	:initarg :hrs
+	:type number)
+  (mins :accessor time-mins
+	:initarg :mins
+	:type number)))
+
+(defmethod print-object ((obj time_c) out) ; __repr__ equivalent
+    (format out "~2,'0D:~2,'0D" (time-hrs obj) (time-mins obj)))
 
 (defun parse-time (str)
   (let ((index (position #\: str)))
-    (make-time_c
+    (make-instance 'time_c
       :hrs (parse-integer (subseq str 0 index))
       :mins (parse-integer (subseq str (+ index 1))))))
 
 (defun calctime (S to_elapse)
-  (let* ((offset (+ (* (time_c-hrs S) 60) (time_c-mins S)))
+  (let* ((offset (+ (* (time-hrs S) 60) (time-mins S)))
   (total (+ offset to_elapse)))
-    (make-time_c
+    (make-instance 'time_c
       :hrs (floor (/ total 60))
       :mins (mod total 60))))
 
@@ -18,6 +27,5 @@
 (setq elapse (parse-integer (pop *args*)))
 (setq end (calctime start elapse))
 
-(format t "start: ~2,'0D:~2,'0D ~@D~%" (time_c-hrs start)
-	(time_c-mins start) elapse)
-(format t "End: ~2,'0D:~2,'0D~%" (time_c-hrs end) (time_c-mins end))
+(format t "Start: ~S ~@D~%End: ~S~%" start elapse end)
+;(format t "End: ~S~%" end)
