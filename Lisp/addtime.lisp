1a7d84ae2bbc284824b17a7314e4c1b6cf6e2824
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-09-20 19:45:08 +0100

    Localised variables

--- Lisp/addtime.lisp
+++ Lisp/addtime.lisp
@@ -1,17 +1,17 @@
 (defstruct time_c hrs mins)
 
 (defun parse-time (str)
-  (setq index (position #\: str))
-  (make-time_c
-   :hrs (parse-integer (subseq str 0 index))
-   :mins (parse-integer (subseq str (+ index 1)))))
+  (let ((index (position #\: str)))
+    (make-time_c
+      :hrs (parse-integer (subseq str 0 index))
+      :mins (parse-integer (subseq str (+ index 1))))))
 
 (defun calctime (S to_elapse)
-  (setq offset (+ (* (time_c-hrs S) 60) (time_c-mins S)))
-  (setq total (+ offset to_elapse))
-  (make-time_c
-   :hrs (floor (/ total 60))
-   :mins (mod total 60)))
+  (let ((offset (+ (* (time_c-hrs S) 60) (time_c-mins S)))
+  (total (+ 10 to_elapse)))
+    (make-time_c
+      :hrs (floor (/ total 60))
+      :mins (mod total 60))))
 
 ; Main
 (setq start (parse-time (pop *args*)))
