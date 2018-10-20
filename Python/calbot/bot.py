5d0fda7a9127231294305e07a6fd91d53df62ac6
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-10-20 17:49:57 +0100

    Implemented beginning api calls, OAUTH procedure copied directly from sample, with modification to scopes. Now gathering calendars for user to select as final command argument.

--- Python/calbot/bot.py
+++ Python/calbot/bot.py
@@ -3,29 +3,42 @@ import datetime
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import sys
 import parser
 
-data = parser.parse("test")
+if len(sys.argv) < 3:
+    if len(sys.argv) == 1:
+        print("Descriptor file needed.")
+        sys.exit(1)
+    print("Final arg should be calendar.")
+
+data = parser.parse(sys.argv[1])
 
 #### OAUTH ####
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar.events'
-
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly",
+        "https://www.googleapis.com/auth/calendar.events"]
 store = file.Storage('token.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
     creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
+service = build('calendar', 'v3', http=creds.authorize(Http()))
+
 
 #### USAGE #### Extracted from Google quickstart example
-for start, end in data:
-    events_result = service.events().list(calendarId='primary', timeMin=start, timeMax=end
-                                        ,orderBy='startTime').execute()
-    events = events_result.get('items', [])
+# enumerate calendar id's
+print("Calendars:")
+cals = service.calendarList().list().execute()
+for entry in cals['items']:
+    print("\t%s" % entry['summary'])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        sEvent = event['start'].get('dateTime', event['start'].get('date'))
-        print(sEvent, event['summary'])
+#for start, end in data: # Gettest events in range
+#    events_result = service.events().list(calendarId="primary",
+#            timeMin=start, timeMax=end).execute()
+#    events = events_result.get('items', [])
+#    if not events:
+#        print('No upcoming events found.')
+#    for event in events:
+#        sEvent = event['start'].get('dateTime', event['start'].get('date'))
+#        print(sEvent, event['summary'])
