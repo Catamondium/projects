059bf50414151d232756d749a98ea765d5dbe982
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-11-18 20:53:47 +0000

    Filled out Bash implementation of addtime to display verbose output and take options.

--- Scripts/Bash/addtime.sh
+++ Scripts/Bash/addtime.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
-function elapse() {
-	local hrs=${1%:*} # delete from ':' onwards
-	local mins=${1#*:} # delete from ':' backwards
-	# calculate mins to add
-	hrs=$(($hrs * 60))
-	local tot=$((hrs + mins + $2))
-	# form hh:mm
-	hrs=$((tot / 60))
-	hrs=${hrs%.*}
-	mins=$((tot % 60))
-	echo $(printf "%02d:%02d" $hrs $mins)
+function sTime() {
+	echo $(printf "%02d:%02d" $1 $2)
 }
 
-if [ $# -lt 2 ]; then
-	printf "Usage: %s hh:mm mins_elapse\n" $0
+function elapse() {
+	# calculate total minutes
+	local offset=$(($1 * 60 + $2))
+	local tot=$((offset + $3))
+	# form hh:mm
+	hrs=$((tot / 60))
+	hrs=${hrs%.*} # floor
+	mins=$((tot % 60))
+	echo $(sTime $hrs $mins)
+}
+
+function usage() {
+	printf "Usage: $0 [-vh] hh:mm mins_elapse\n"
+	printf "Options:\n\t-v disable verbosity\n"
+	printf "\t-h print this message and exit\n"
 	exit 1
+}
+
+v=true
+while getopts "vh" c; do
+	case "${c}" in
+		v)
+			v=false
+			;;
+		h | *)
+			usage
+			;;
+		esac
+	done
+shift $((OPTIND-1))
+
+if [ $# -lt 2 ]; then
+	usage
 fi
 
-elapse $1 $2
+s_hrs=${1%:*} # Delete from ':' onwards
+s_mins=${1#*:} # Delete from ':' backwards
+e_time=$(elapse $s_hrs $s_mins $2)
+
+if $v ; then
+	s_time=$(sTime $s_hrs $s_mins)
+	printf "Start:\t%s\t%+d\nEnd:\t%s\n" $s_time $2 $e_time
+else
+	echo $e_time
+fi
