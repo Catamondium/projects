f1ef1974efba3557b2b68932441e2a8d89b8bc43
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-11-18 23:20:15 +0000

    .

--- Scripts/Bash/addtime.sh
+++ Scripts/Bash/addtime.sh
@@ -16,7 +16,7 @@ function elapse() {
 }
 
 function usage() {
-	printf "Usage: $0 [-vh] hh:mm mins_elapse\n"
+	printf "Usage: $0 [-qh] hh:mm mins_elapse\n"
 	printf "Options:\n\t-q quietly output end time\n"
 	printf "\t-h print this message and exit\n"
 	exit 1
