734dba87fd9fc655f108ae1cf2dd1dfe209dad5e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2018-12-16 20:53:28 +0000

    Makefile: formatting and datafile initialisation. main.cpp: now accessing ~/.notes for datafile instead of being directly passed ./notes.

--- Cpp/nn/main.cpp
+++ Cpp/nn/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <unistd.h> // *nix api
+#include <sys/types.h> // *nix types
+#include <pwd.h> // working directory stuff
 #include "parsing.hpp"
 #include "note.hpp"
 
@@ -12,13 +15,22 @@
  * Notification system
  */
 
+std::string getHome() {
+	char *homedir;
+	if((homedir = getenv("HOME")) == NULL)
+		homedir = getpwuid(getuid())->pw_dir;
+
+	return std::string(homedir);
+}
+
 int main(int argc, char **argv) {
-	if(argc < 2) {
+	std::cout << getHome() + "/.notes" << std::endl;
+	/*if(argc < 2) {
 		std::cout << "notes file required" << std::endl;
 		std::exit(1);
-	}
+	}*/
 
-	std::vector<Note> notes = parsing::parse(argv[1]);
+	std::vector<Note> notes = parsing::parse(getHome()+"/.notes");//argv[1]);
 
 	for(auto note : notes) {
 		std::cout << "Heading:\t" << note.getHeading() << std::endl;
