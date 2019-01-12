c397752a2aa00302eaf36d2ff5dfcaee5f195654
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-12 21:25:37 +0000

    Added firefox extension to download online images open raw in tabs

---
+++ JS/dtimgs/bg.js
@@ -0,0 +1,27 @@
+console.log("BG");
+
+var re = new RegExp(".+(.jpg|.jpeg|.png)$", "i");
+
+function runtab(tabs) {
+	console.log("PRESSED");
+	let imgtabs = tabs.filter(tab => re.test(tab.url));
+	console.log(imgtabs);
+	for(let tab of imgtabs) {
+		console.log(`Found: ${tab.url}`);
+		fname = tab.url.slice(tab.url.lastIndexOf('/'));
+		browser.downloads.download(
+			{
+				url: tab.url,
+				filename: `dir/${fname}`
+			});
+	}
+}
+
+function onErr(err) {
+	console.error(err);
+}
+
+browser.browserAction.onClicked.addListener(() => {
+	browser.tabs.query({currentWindow: true})
+		.then(runtab, onErr);
+});
