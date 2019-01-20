5fefbd095a6ee23e5fb2c4807b1aabd5890bbdd9
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-20 15:19:19 +0000

    Now accepting more regex grammars according to a few of the available flag constants.

--- Cpp/reSort.cpp
+++ Cpp/reSort.cpp
@@ -8,21 +8,35 @@
 void usage(const std::string prog)
 {
 	std::cout <<
-		"usage: " << prog << " [-ir:] <regex> <strings>*\n"
-		"options:\n\t-i invert group order" << std::endl;
+		"usage: " << prog << " [-ipagh] <regex> <strings>*\n"
+		"options:\n\t-i invert group order\n"
+		"\t-p POSIX regex\n"
+		"\t-a AWK regex\n"
+		"\t-g GREP regex\n"
+		"\t-h print this usage" << std::endl;
 	std::exit(1);
 }
 
 int main(int argc, char **argv)
 {
 	bool invert = false;
+	auto mode = std::regex::ECMAScript;
 	std::regex re;
 
 	int c;
-	while((c = getopt(argc, argv, "ih")) != -1) {
+	while((c = getopt(argc, argv, "ipagh")) != -1) {
 		switch(c) {
 			case 'i':
 				invert = true;
+				break;
+			case 'p':
+				mode = std::regex::basic;
+				break;
+			case 'a':
+				mode = std::regex::awk;
+				break;
+			case 'g':
+				mode = std::regex::grep;
 				break;
 			default:
 				usage(argv[0]);
@@ -33,8 +47,14 @@ int main(int argc, char **argv)
 	std::vector<std::string> unmatched;
 
 	if((argc - optind) < 1) usage(argv[0]);
-	else
-		re = argv[optind++];
+	else {
+		try {
+			re = std::regex(argv[optind++], mode);
+		} catch (const std::regex_error& e) {
+			std::cout << e.what() << std::endl;
+			std::exit(1);
+		}
+	}
 
 	for(int i = optind; i < argc; ++i) {
 		std::cmatch m;
