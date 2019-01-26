a1037943f563d27193bebea95efde609b3bd1103
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-26 12:27:26 +0000

    cleaned up std::regex using re. Now flagging to ignore submatches.

--- Cpp/reSort.cpp
+++ Cpp/reSort.cpp
@@ -13,7 +13,7 @@ void usage(const std::string prog)
 		"\t-i invert group order\n"
 		"\t-c case-insensitive\n"
 		"\t-h print this usage\n"
-		"\tRegex grammars:\n"
+		"\tregex grammars:\n"
 		"\t\t-j JS / ECMAScript (default)\n"
 		"\t\t-p POSIX\n"
 		"\t\t-e POSIX extended\n"
@@ -24,8 +24,9 @@ void usage(const std::string prog)
 
 int main(int argc, char **argv)
 {
+	using re = std::regex;
 	bool invert = false, match_case = true;
-	auto mode = std::regex::ECMAScript;
+	auto mode = re::ECMAScript;
 
 	int c;
 	while((c = getopt(argc, argv, "ichjpegx")) != -1) {
@@ -36,35 +37,35 @@ int main(int argc, char **argv)
 			case 'c':
 				match_case = false;
 			case 'j':
-				mode = std::regex::ECMAScript;
+				mode = re::ECMAScript;
 				break;
 			case 'p':
-				mode = std::regex::basic;
+				mode = re::basic;
 				break;
 			case 'e':
-				mode = std::regex::awk;
+				mode = re::awk;
 				break;
 			case 'g':
-				mode = std::regex::grep;
+				mode = re::grep;
 				break;
 			case 'x':
-				mode = std::regex::egrep;
+				mode = re::egrep;
 				break;
 			default:
 				usage(argv[0]);
 		}
 	}
 
-	if(!match_case) mode |= std::regex::icase;
-	mode |= std::regex::optimize; // Only making 1 regex, more to scan
+	if(!match_case) mode |= re::icase;
+	mode |= re::nosubs | re::optimize; // Optimise for detection
 
-	std::regex re;
+	re pattern;
 	std::vector<std::string> matched, unmatched;
 
 	if((argc - optind) < 1) usage(argv[0]);
 
 	try {
-		re = std::regex(argv[optind++], mode);
+		pattern = re(argv[optind++], mode);
 	} catch (const std::regex_error& e) {
 		std::cout << e.what() << std::endl;
 		std::exit(1);
@@ -72,7 +73,7 @@ int main(int argc, char **argv)
 
 	for(int i = optind; i < argc; ++i) {
 		std::cmatch m;
-		if(regex_match(argv[i], m, re))
+		if(regex_match(argv[i], m, pattern))
 			matched.push_back(argv[i]);
 		else
 			unmatched.push_back(argv[i]);
