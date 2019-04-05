86799223f2cccb0d924b0461e7f9bc77bee87db9
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-05 17:07:01 +0100

    .

--- Python/mol/mol.py
+++ Python/mol/mol.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     for comp in sys.argv[1:]:
         clean = sanitize(comp)
         try:
-            print("%s:\t%.2f" % (clean, mass(clean)))
+            print("%s:\t%.2f g/mol" % (clean, mass(clean)))
         except Exception as e:
             print(e)
             exit(1)
