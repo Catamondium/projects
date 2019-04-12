7602397b4285c6bd9f40f1f77c6d13351e9b4b56
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-12 23:41:36 +0100

    Implemented proper error handling w/ custom type

--- Rust/mol/src/lib.rs
+++ Rust/mol/src/lib.rs
@@ -1,5 +1,8 @@
 extern crate regex;
 use regex::Regex;
+
+use std::fmt;
+use std::error;
 #[macro_use]
 extern crate lazy_static;
 
@@ -13,11 +16,16 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
     fn none_element() {
-        mass(&"5Haag5".to_string());
+        let badelem=  "Haag";
+        let res: Result<f32, ElementError> = mass(&badelem.to_string());
+        assert!(res.is_err(), format!("\"{}\"Failed to fail", badelem));
+        if let Err(e) = res {
+            let estr = format!("{}", e);
+            assert!(estr.contains(badelem), "Failed to produce description");
+        }
     }
-
+    
     #[test]
     fn normalization() {
         let dirty: String = "C   5#[20]{30}".to_string();
@@ -26,18 +34,19 @@ mod tests {
 
     #[test]
     fn element_carbon() {
-        assert_eq!(12.01, mass(&"C".to_string()));
+        assert_eq!(12.01, mass(&"C".to_string()).unwrap());
     }
 
     #[test]
     fn simple_ethane() {
-        assert_eq!(30.02, mass(&"C2H6".to_string()));
+        assert_eq!(30.02, mass(&"C2H6".to_string()).unwrap());
     }
 
     #[test]
     fn five_struct_dimethylpropane() {
-        assert_eq!(5.0 * 72.05, mass(&"5C(CH3)4".to_string()));
+        assert_eq!(5.0 * 72.05, mass(&"5C(CH3)4".to_string()).unwrap());
     }
+
 }
 
 const PTABLE_FILE: &str = include_str!("ptable.tsv");
@@ -48,6 +57,25 @@ lazy_static! {
     static ref COEFFRE: Regex = Regex::new(r"^(\d+)").unwrap();  // beginning coefficient
     static ref TOKRE:   Regex = Regex::new(r"\(.*?\)|([A-Z][a-z]*)(\d*)").unwrap();  // groups: Elem, [coeff]
     static ref SUBRE:   Regex = Regex::new(r"\((.*)\)(\d*)").unwrap(); // groups: expr, [coeff]
+}
+
+#[derive(Debug, Clone)]
+pub struct ElementError(String);
+
+impl fmt::Display for ElementError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Element \"{}\" doesn't exist", self.0)
+    }
+}
+
+impl error::Error for ElementError {
+    fn description(&self) -> &str {
+        "The element entered doesn't exist"
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
 }
 
 fn make_coeff(src: std::option::Option<regex::Match<'_>>) -> f32 {
@@ -99,7 +127,7 @@ pub fn normalize(dirty: String) -> String {
         .collect()
 }
 
-pub fn mass(comp: &String) -> f32 {
+pub fn mass(comp: &String) -> Result<f32, ElementError> {
     let bigcoeff = match COEFFRE.captures(&comp) {
         Some(x) => x[1].parse::<f32>().unwrap_or(1.0),
         None => 1.0,
@@ -114,8 +142,7 @@ pub fn mass(comp: &String) -> f32 {
             if let Some(efloat) = PTABLE.get(key) {
                 temp = *efloat;
             } else {
-                let err = format!("Element {} doesn't exist", key);
-                panic!(err);
+                Err(ElementError(key.to_string()))?;
             }
         }
 
@@ -126,12 +153,12 @@ pub fn mass(comp: &String) -> f32 {
     for cap in SUBRE.captures_iter(&comp) {
         let mut temp = 0.0;
         if let Some(subexpr) = cap.get(1) {
-            temp = mass(&subexpr.as_str().to_string());
+            temp = mass(&subexpr.as_str().to_string())?;
         }
 
         temp *= make_coeff(cap.get(2));
         acc += temp;
     }
 
-    bigcoeff * acc
+    Ok(bigcoeff * acc)
 }
--- Rust/mol/src/main.rs
+++ Rust/mol/src/main.rs
@@ -1,10 +1,18 @@
 use mol::{mass, normalize};
-use std::env;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     for dirty in args {
         let clean = normalize(dirty);
-        println!("{}: {} g/mol", clean, mass(&clean));
+        match mass(&clean) {
+            Ok(m)   => {
+                println!("{}: {} g/mol", clean, m);
+                },
+            Err(e)  => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        }
     }
 }
