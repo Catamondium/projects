fb53f34ae6571e4a7adaea31d5dba9db798fa00e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-17 02:44:35 +0100

    .

--- Rust/rname/src/main.rs
+++ Rust/rname/src/main.rs
@@ -4,22 +4,26 @@ use regex::{escape, Regex};
 use std::path::*;
 use std::{env, fs};
 
-fn re_sort<'a>(dir: &'a Vec<PathBuf>, parent: &Path) -> Vec<&'a PathBuf> {
-    let width = (dir.len() as f64).log10().ceil();
-    let re_string = format!(
-        r"^{}-\d{{{}}}.*$",
-        escape(&parent.file_name().unwrap().to_str().unwrap()),
-        width
-    );
+fn re_sort<'a>(dir: &'a Vec<PathBuf>, parent: &str, width: &f64) -> Vec<&'a PathBuf> {
+    let re_string = format!(r"^{}-\d{{{}}}.*$", escape(&parent), width);
     let re = Regex::new(&re_string).unwrap();
 
-    let (mut matched, mut unmatched): (Vec<&PathBuf>, Vec<&PathBuf>) = dir
-        .iter()
-        .partition(|e| re.is_match(e.file_name().unwrap().to_str().unwrap()));
+    let (mut matched, mut unmatched): (Vec<&PathBuf>, Vec<&PathBuf>) = dir.iter().partition(|e| {
+        if let Some(x) = e.file_name() {
+            if let Some(y) = x.to_str() {
+                return re.is_match(y);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    });
 
     println!("Re: {:?}", re);
     println!("Matched: {:?}\nUnmatched: {:?}", matched, unmatched);
 
+    matched.sort();
     unmatched.append(&mut matched);
 
     unmatched
@@ -49,13 +53,25 @@ fn rename(reldir: &Path) {
         let (dirs, files): (Vec<fs::DirEntry>, Vec<fs::DirEntry>) =
             filtered.partition(is_directory);
 
-        for d in dirs {
+        for _d in dirs {
             //rename(&d.path()); // Don't recurse when testing
         }
 
         let fpaths: Vec<PathBuf> = files.iter().map(|f| f.path()).collect();
-        re_sort(&fpaths, &dir);
-        for f in fpaths {}
+        let width = (fpaths.len() as f64).log10().ceil();
+        let dirname = dir.file_name().unwrap().to_str().unwrap();
+
+        let sorted = re_sort(&fpaths, &dirname, &width);
+
+        for (i, f) in sorted.iter().enumerate() {
+            let ext = match f.extension() {
+                Some(e) => e.to_str().unwrap_or(""),
+                None => "",
+            };
+
+            let newname = format!("{}-{:01}.{}", dirname, i, ext);
+            println!("{:?} -> \"{}\"", f.file_name().unwrap(), newname);
+        }
     }
 }
 
