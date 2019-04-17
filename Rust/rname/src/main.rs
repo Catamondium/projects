05d3f842ffa619a90404e0ee73162c53399e6c78
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-17 14:40:35 +0100

    full impl

--- Rust/rname/src/main.rs
+++ Rust/rname/src/main.rs
@@ -1,10 +1,22 @@
 extern crate regex;
 use regex::{escape, Regex};
 
+extern crate getopts;
+use getopts::Options;
+
 use std::path::*;
 use std::{env, fs};
 
-fn re_sort<'a>(dir: &'a Vec<PathBuf>, parent: &str, width: &f64) -> Vec<&'a PathBuf> {
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (args, config) = target(&args);
+
+    for x in args {
+        mv(Path::new(&x), &config);
+    }
+}
+
+fn re_sort<'a>(dir: &'a Vec<PathBuf>, parent: &str, width: &usize) -> Vec<&'a PathBuf> {
     let re_string = format!(r"^{}-\d{{{}}}.*$", escape(&parent), width);
     let re = Regex::new(&re_string).unwrap();
 
@@ -20,80 +32,109 @@ fn re_sort<'a>(dir: &'a Vec<PathBuf>, parent: &str, width: &f64) -> Vec<&'a Path
         }
     });
 
-    println!("Re: {:?}", re);
-    println!("Matched: {:?}\nUnmatched: {:?}", matched, unmatched);
-
     matched.sort();
-    unmatched.append(&mut matched);
+    matched.append(&mut unmatched);
 
-    unmatched
+    matched
 }
 
-fn is_directory(thing: &fs::DirEntry) -> bool {
-    if let Ok(typename) = thing.file_type() {
+fn is_directory(file: &fs::DirEntry) -> bool {
+    if let Ok(typename) = file.file_type() {
         return typename.is_dir();
     }
     false
 }
 
-fn rename(reldir: &Path) {
-    if !reldir.is_dir() {
+fn mv(rel_parent: &Path, conf: &Config) {
+    if !rel_parent.is_dir() {
         return;
     }
 
-    let dir = reldir.canonicalize().unwrap();
+    let parent = rel_parent.canonicalize().unwrap();
 
-    println!(
-        "in Path: {:?}",
-        dir.canonicalize().unwrap().file_name().unwrap()
-    );
-
-    if let Ok(iter) = fs::read_dir(&dir) {
+    if let Ok(iter) = fs::read_dir(&parent) {
         let filtered = iter.filter_map(|x| x.ok());
         let (dirs, files): (Vec<fs::DirEntry>, Vec<fs::DirEntry>) =
             filtered.partition(is_directory);
 
-        for _d in dirs {
-            //rename(&d.path()); // Don't recurse when testing
-        }
-
         let fpaths: Vec<PathBuf> = files.iter().map(|f| f.path()).collect();
-        let width = (fpaths.len() as f64).log10().ceil();
-        let dirname = dir.file_name().unwrap().to_str().unwrap();
+        let width = (fpaths.len() as f64).log10().ceil().abs() as usize;
+        let dirname = parent.file_name().unwrap().to_str().unwrap();
 
         let sorted = re_sort(&fpaths, &dirname, &width);
+        println!("{:#?}", sorted);
 
         for (i, f) in sorted.iter().enumerate() {
             let ext = match f.extension() {
-                Some(e) => e.to_str().unwrap_or(""),
-                None => "",
+                Some(e) => format!("{}{}", ".", e.to_str().unwrap_or("")),
+                None => String::new(),
             };
 
-            let newname = format!("{}-{:01}.{}", dirname, i, ext);
-            println!("{:?} -> \"{}\"", f.file_name().unwrap(), newname);
+            let newname = format!("{}-{:0width$}{}", dirname, i, ext, width = width);
+            if conf.verbose {
+                println!("{:?} -> \"{}\"", f.file_name().unwrap(), newname);
+            }
+
+            if !conf.dry {
+                let _r = fs::rename(f, parent.join(Path::new(&newname)));
+            }
+        }
+
+        if conf.recurse {
+            for d in dirs {
+                mv(&d.path(), &conf); // Don't recurse when testing
+            }
         }
     }
 }
 
-fn target(args: &Vec<String>) -> Vec<String> {
-    if args.len() == 0 {
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub dry: bool,
+    pub verbose: bool,
+    pub recurse: bool,
+    pub force: bool,
+}
+
+fn usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {}\nBatch renamer following DIR-#.ext pattern.\n# is an integer zfilled to width log10 of the number of files to mv recursing directories", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn target(args: &Vec<String>) -> (Vec<String>, Config) {
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("d", "dry_run", "Run without renaming anything, verbosely");
+    opts.optflag("v", "verbose", "Show all possible moves, verbosely");
+    opts.optflag("r", "recurse", "Recurse through directories");
+    opts.optflag("f", "force", "Rename without asking permission");
+    opts.optflag("i", "interactive", "Rename while asking permission");
+    opts.optflag("h", "help", "show usage");
+    let matches = opts.parse(&args[1..]).unwrap_or_else(|e| {
+        eprintln!("Bad option: {}", e);
+        usage(&program, &opts);
+        std::process::exit(1);
+    });
+
+    if matches.opt_present("h") {
+        usage(&program, &opts);
+        std::process::exit(0);
+    }
+
+    let conf = Config {
+        dry: matches.opt_present("d"),
+        verbose: matches.opt_present("d") || matches.opt_present("v"),
+        recurse: matches.opt_present("r"),
+        force: matches.opt_present("f") || matches.opt_present("i"),
+    };
+
+    let mut ret_vec = matches.free;
+    if ret_vec.len() == 0 {
         if let Ok(pwd) = env::var("PWD") {
-            return vec![pwd];
-        } else {
-            return Vec::new();
+            ret_vec.push(pwd);
         }
-    } else {
-        return args.clone();
     }
-}
 
-fn main() {
-    let mut args: Vec<String> = env::args().skip(1).collect();
-    args = target(&args);
-
-    println!("Args: {:?}", args);
-
-    for x in args {
-        rename(Path::new(&x));
-    }
+    (ret_vec, conf)
 }
