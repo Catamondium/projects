7339c78234eb9fc89ab6c4ef150d62725fccf3af
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-20 16:03:45 +0100

    .

---
+++ Rust/rname_daemon/src/main.rs
@@ -0,0 +1,52 @@
+extern crate inotify;
+use inotify::{Inotify, WatchDescriptor, WatchMask};
+
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use std::path::*;
+
+use rname_daemon::*;
+
+type WatchMap = HashMap<WatchDescriptor, PathBuf>;
+
+const CONF: Config = Config {
+    verbose: false,
+    dry: false,
+    recurse: false,
+};
+
+fn to_path(args: &Vec<String>) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    let mut vec = Vec::new();
+    for x in args {
+        vec.push(Path::new(&x).canonicalize()?);
+    }
+
+    Ok(vec)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let paths = to_path(&env::args().skip(1).collect())?;
+
+    let mut inotify = Inotify::init()?;
+
+    let mut map: WatchMap = HashMap::new();
+    for p in paths {
+        let c = p.clone();
+        let wd = inotify.add_watch(p, WatchMask::CREATE | WatchMask::DELETE)?;
+        map.insert(wd, c);
+    }
+
+    loop {
+        let mut buf = [0; 1024];
+        let events = inotify.read_events_blocking(&mut buf)?;
+
+        for e in events {
+            let caller = map.get(&e.wd).unwrap();
+            println!("Event: {:?}", e.mask);
+            println!("name: {:?}", caller);
+
+            mv(&caller, &CONF)?;
+        }
+    }
+}
