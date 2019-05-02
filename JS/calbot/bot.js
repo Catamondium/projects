ebba19f807410e2c1b6a89663db9bf18ccde4d77
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-02 22:43:21 +0100

    .

--- JS/calbot/bot.js
+++ JS/calbot/bot.js
@@ -2,20 +2,20 @@
 
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis'); // {} cast to object
+const { google } = require('googleapis'); // {} cast to object
 const parse = require('./parser.js').parse;
 
 // If modifying these scopes, delete token.json.
 const SCOPES = [
-"https://www.googleapis.com/auth/calendar.readonly",
-"https://www.googleapis.com/auth/calendar.events"];
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/calendar.events"];
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
-	if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-	authorize(JSON.parse(content), main);
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), main);
 });
 
 /**
@@ -25,16 +25,16 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-	const {client_secret, client_id, redirect_uris} = credentials.installed;
-	const oAuth2Client = new google.auth.OAuth2(
-		client_id, client_secret, redirect_uris[0]);
-	
-	// Check if we have previously stored a token.
-	fs.readFile(TOKEN_PATH, (err, token) => {
-		if(err) return getAccessToken(oAuth2Client, callback);
-		oAuth2Client.setCredentials(JSON.parse(token));
-		callback(oAuth2Client);
-	});
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
 }
 
 /**
@@ -44,124 +44,132 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getAccessToken(oAuth2Client, callback) {
-	const authUrl = oAuth2Client.generateAuthUrl({
-		access_type: 'offline',
-		scope: SCOPES,
-	});
-	console.log('Authorize this app by visiting this url:', authUrl);
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	rl.question('Enter the code from that page here: ', (code) => {
-		rl.close();
-		oAuth2Client.getToken(code, (err, token) => {
-			if (err) return console.error('Error retrieving access token', err);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-				if (err) console.error(err);
-				console.log('Token stored to', TOKEN_PATH);
-			});
-			callback(oAuth2Client);
-		});
-	});
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
 }
 
 function logCals(api) {
-	api.calendarList.list({showHidden : true},
-		(err, cals) => { // Never called
-			if (err) throw err;
-			sums = cals.data.items.map(x => x = x.summary);
-			console.log("Calendars:");
-			sums.forEach(item => {
-				console.log(`\t${item}`);
-			});
-		}
-	);
+    api.calendarList.list({ showHidden: true, minAccessRole: "writer" },
+        (err, cals) => { // Never called
+            if (err) throw err;
+            sums = cals.data.items.map(x => x = x.summary);
+            console.log("Calendars:");
+            sums.forEach(item => {
+                console.log(`\t${item}`);
+            });
+        }
+    );
 }
 
 function simplify(item) {
-	ret = {sum: item.summary,
-		rec: item.recurrence,
-		id: item.id};
-	return ret;
+    ret = {
+        sum: item.summary,
+        rec: item.recurrence,
+        id: item.id
+    };
+    return ret;
 }
 
 function delEvents(api, name, data) {
-	api.calendarList.list({showHidden : true},
-		(err, cals) => {
-			if (err) throw err;
-			calIDs = cals.data.items.map(x => x = simplify(x));
-			let cal;
-			
-			calIDs.some(item => {
-				if (item.sum == name) {
-					cal = item.id;
-					return true
-				}});
-			
-			if (!cal) {
-				console.log("Calendar not found.");
-				logCals(api);
-				process.exit(1);
-			} else
-				getEvents(api, cal, data);
-		}
-	);
+    api.calendarList.list({ showHidden: true, minAccessRole: "writer" },
+        (err, cals) => {
+            if (err) throw err;
+            calIDs = cals.data.items.map(x => x = simplify(x));
+            let cal;
+
+            calIDs.some(item => {
+                if (item.sum == name) {
+                    cal = item.id;
+                    return true
+                }
+            });
+
+            if (!cal) {
+                console.log("Calendar not found.");
+                logCals(api);
+                process.exit(1);
+            } else
+                getEvents(api, cal, data);
+        }
+    );
 }
 
 function getEvents(api, cal, data) {
-	data.forEach(hol => {
-		api.events.list({
-			calendarId: cal,
-			timeMin: hol.start,
-			timeMax: hol.end},
-			(err, events) => {
-				if (err) throw err;
-				eventIDs =  new Set(events.data.items.map(
-					x => x = simplify(x)));
-				eventIDs.forEach(ev => {
-					if (ev.rec) {
-						expand(api, cal, ev, hol)
-					}
-				})}
-		)})
+    data.forEach(hol => {
+        api.events.list({
+            calendarId: cal,
+            timeMin: hol.start,
+            timeMax: hol.end
+        },
+            (err, events) => {
+                if (err) throw err;
+                eventIDs = new Set(events.data.items.map(
+                    x => x = simplify(x)));
+                eventIDs.forEach(ev => {
+                    if (ev.rec) {
+                        expand(api, cal, ev, hol)
+                    }
+                })
+            }
+        )
+    })
 }
 
 function expand(api, cal, ev, hol) {
-	api.events.instances({
-		calendarId: cal,
-		eventId: ev.id,
-		timeMin: hol.start,
-		timeMax: hol.end},
-		(err, events) => {
-			if (err) throw err;
-			eventIDs = events.data.items.map(
-				x => x = x.id);
-			del(api, cal, eventIDs);
-		});
+    api.events.instances({
+        calendarId: cal,
+        eventId: ev.id,
+        timeMin: hol.start,
+        timeMax: hol.end
+    },
+        (err, events) => {
+            if (err) throw err;
+            eventIDs = events.data.items.map(
+                x => x = x.id);
+            del(api, cal, eventIDs);
+        });
 }
 
 function del(api, cal, events) {
-	events.forEach(ev => {
-		api.events.delete({
-			calendarId: cal,
-			eventId: ev});
-	});
+    events.forEach(ev => {
+        api.events.delete({
+            calendarId: cal,
+            eventId: ev
+        });
+    });
 }
 
 function main(auth) {
-	const calendar = google.calendar({version: 'v3', auth});
-	let args = process.argv.slice(2);
-	if (args.length == 0) {
-		console.log("Usage: ./bot.js spec calendar");
-		process.exit(1);
-	} else if (args.length == 1) {
-		logCals(calendar);
-		process.exit(1);
-	}  else {
-		let dat = parse(args[0]);
-		delEvents(calendar, args[1], dat);
-	}
+    const calendar = google.calendar({ version: 'v3', auth });
+    let args = process.argv.slice(2);
+    if (args.length == 0) {
+        console.log("Usage: ./bot.js spec calendar");
+        process.exit(1);
+    } else if (args.length == 1) {
+        logCals(calendar);
+        process.exit(1);
+    } else {
+        let dat = parse(args[0]);
+        delEvents(calendar, args[1], dat);
+    }
 }
--- JS/calbot/parser.js
+++ JS/calbot/parser.js
@@ -1,24 +1,24 @@
 const readFileSync = require('fs').readFileSync;
 
-exports.gdate = function(ukdate) {
-	// DD/MM/YYYY parser
-	let segs = ukdate.split('/');
-	segs = segs.map(n => parseInt(n));
-	return new Date(segs[2], segs[1]-1, segs[0]).toISOString();
+exports.gdate = function (ukdate) {
+    // DD/MM/YYYY parser
+    let segs = ukdate.split('/');
+    segs = segs.map(n => parseInt(n));
+    return new Date(segs[2], segs[1] - 1, segs[0]).toISOString();
 }
 
-exports.parse = function(file) {
-	let ret = new Array();
-	data = readFileSync(file).toString();
-	let pairs = data.split('\n');
-	pairs.forEach((pair) => {
-		if (pair != '') {
-			let dates = pair.split(' ');
-			ret.push({
-				start: exports.gdate(dates[0]),
-				end: exports.gdate(dates[1])
-			});
-		}
-	})
-	return ret;
+exports.parse = function (file) {
+    let ret = new Array();
+    data = readFileSync(file).toString();
+    let pairs = data.split('\n');
+    pairs.forEach((pair) => {
+        if (pair != '') {
+            let dates = pair.split('\t');
+            ret.push({
+                start: exports.gdate(dates[0]),
+                end: exports.gdate(dates[1])
+            });
+        }
+    })
+    return ret;
 }
