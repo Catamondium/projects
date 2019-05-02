e55c97fcc56bae77d5535939e7b9ad0d24f68013
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-02 22:51:54 +0100

    .

--- Python/calbot/bot.py
+++ Python/calbot/bot.py
@@ -1,10 +1,7 @@
 #!./bin/python3
 import datetime
 import pickle
-
-# import os.path
 from pathlib import Path
-
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
