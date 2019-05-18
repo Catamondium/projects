5bd2cc7d2b6fdfa9c857bcd5ce60ab53ccb59a20
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-18 23:40:13 +0100

    .

--- Ruby/rname.rb
+++ Ruby/rname.rb
@@ -1,31 +1,13 @@
 #!/usr/bin/ruby
 require 'pathname'
-Config = Struct.new(:dry, :verbose, :recurse, :force) do
-    def self.parseopts(opts)
-        dry = opts.include?('-d')     ? true : false
-        verbose = opts.include?('-v') ? true : false
-        recurse = opts.include?('-r') ? true : false
-        force = opts.include?('-f') ? true : false
-
-        if dry
-            verbose = true
-        end
-
-        self.new(dry, verbose, recurse, force)
-    end
-end
-
-def approve(path)
-    print "Rename inside? #{path} "
-    char = STDIN.gets.chomp[0]&.downcase or 'y'
-    return char == 'y'
-end
+require 'optparse'
+require 'ostruct'
 
 def re_sort(dir, parent, width)
     re = /^#{Regexp.escape(parent)}-\d{#{width}}/
     matched, unmatched = dir
-        .partition {|f| f.basename.to_s =~ re}
-        .map {|fs| fs.sort}
+    .partition {|f| f.basename.to_s =~ re}
+    .map {|fs| fs.sort}
     return matched |= unmatched
 end
 
@@ -33,34 +15,73 @@ def mv(parent, conf)
     path = Pathname.new(parent).expand_path
     files, dirs = path.each_child
         .partition {|f| f.file?}
-
-    if files.length != 0
-        width = Math::log10(files.length).ceil
-        files = re_sort(files, path.basename.to_s, width)
-        files.each_with_index do |f, i|
-            n = "%s-%0*d%s" % [path.basename, width, i, f.extname]
-            npath = path / n
-            if npath == f
-                next
-            end
-            if conf.verbose
-            puts "#{f.basename.to_s} -> #{n}"
-            end
-
-            if not conf.dry
-                f.rename(npath)
+        
+        if files.length != 0
+            width = Math::log10(files.length).ceil
+            files = re_sort(files, path.basename.to_s, width)
+            files.each_with_index do |f, i|
+                n = "%s-%0*d%s" % [path.basename, width, i, f.extname]
+                npath = path / n
+                if npath == f
+                    next
+                end
+                if conf.verbose
+                    puts "#{f.basename.to_s} -> #{n}"
+                end
+                
+                if not conf.dry
+                    f.rename(npath)
+                end
             end
         end
-    end
 
     if conf.recurse
         dirs.each {|d| mv(d, conf)}
     end
 end
 
-opts, args = ARGV.partition {|x| x =~ /^-.*/}
-conf = Config::parseopts(opts)
-args.each do |a|
+def approve(path)
+    print "Rename inside #{path}? "
+    char = STDIN.gets.chomp[0]&.downcase or 'y'
+    return char == 'y'
+end
+
+def parse(args)
+    options = OpenStruct.new
+    options.dry = false
+    options.verbose = false
+    options.recurse = false
+    options.force = false
+
+    parser = OptionParser.new do |opts|
+        opts.banner = "Usage: #{$0} [-vfdr] [DIRS]..."
+
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        opts.on("-v", "--[no-]verbose", "Print moves") do |v|
+            options.verbose = v
+        end
+        opts.on("-f", "--[no-]force", "don't ask about argument DIRS") do |f|
+            options.force = f
+        end
+        opts.on("-r", "--[no-]recurse", "rename recursively") do |r|
+            options.recurse = r
+        end
+        opts.on("-d", "--[no-]dry", "Print moves") do |d|
+            options.verbose |= d
+            options.force &= !d
+            options.dry = d
+        end
+    end
+
+    parser.parse!(args)
+    return options
+end
+
+conf = parse(ARGV)
+
+ARGV.each do |a|
     if !conf.force and !approve(a)
         next
     end
