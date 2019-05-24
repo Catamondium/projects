913db0a92708bc018ba53ba6d3e30d6af23ba5a9
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-24 17:02:51 +0100

    .

--- Kotlin/addtime.kt
+++ Kotlin/addtime.kt
@@ -1,22 +1,54 @@
+import kotlin.system.exitProcess
+
 class Time(val hrs: Int, val mins: Int) {
-    constructor(s: String): this(s.split(':')) // must delegate?
-    // Guess we're supporting List<String> too
+    constructor(s: String): this(s.split(':'))
     constructor(s: List<String>): this(s.getOrNull(0)?.toInt() ?: 0, s.getOrNull(1)?.toInt() ?: 0)
+    constructor(m: Int): this((m/60).toInt(), m % 60)
     override fun toString(): String {
       return "%02d:%02d".format(hrs, mins)
     }
 
-    fun abs(): Int {
-        return hrs*60 + mins
-    }
+    val abs = hrs*60 + mins
 }
+
 operator fun Time.plus(mins_in: Int): Time {
-    val tot = this.abs() + mins_in
-    return Time((tot/60).toInt(), tot % 60)
+    val tot = this.abs + mins_in
+    return Time(tot)
+}
+
+operator fun Time.plus(other: Time): Time {
+    val tot = this.abs + other.abs
+    return Time(tot)
+}
+
+fun usage() {
+    exitProcess(1)
+}
+
+fun fromString(str: String): Time {
+    if (':' in str) {
+        return Time(str)
+    } else {
+        return Time(str.toInt())
+    }
 }
 
 fun main(args: Array<String>) {
-    val t = Time(1, 30) + 30
-    println(t)
-    println(Time("1"))
+    val re = "^-[a-zA-Z]+".toRegex()
+    val (opts, free) = args.partition{thing -> re.matches(thing)}
+
+    if ("-h" in opts || free.size < 2) {
+        println("exit")
+        usage()
+    }
+
+    val (str_start, str_elapse) = free
+    val start = fromString(str_start)
+    val elapse = fromString(str_elapse)
+
+    if (opts.contains("-q")) {
+        println("${start + elapse}")
+    } else {
+        println("Start:\t$start\t${elapse.abs}\nEnd:\t${start + elapse}")
+    }
 }
