c5e3487e8de87931fbf889e27a5d5ba7b39163e4
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-24 18:48:19 +0100

    .

--- Kotlin/addtime.kt
+++ Kotlin/addtime.kt
@@ -1,31 +1,40 @@
 import kotlin.system.exitProcess
 
 class Time(val hrs: Int, val mins: Int) {
+    val abs = hrs*60 + mins
+    
     constructor(s: String): this(s.split(':'))
-    constructor(s: List<String>): this(s.getOrNull(0)?.toInt() ?: 0, s.getOrNull(1)?.toInt() ?: 0)
+    constructor(s: List<String>) : this(s.getOrNull(0)?.toInt() ?: 0, s.getOrNull(1)?.toInt() ?: 0)
     constructor(m: Int): this((m/60).toInt(), m % 60)
-    override fun toString(): String {
+    
+    override fun toString() : String {
       return "%02d:%02d".format(hrs, mins)
     }
 
-    val abs = hrs*60 + mins
 }
 
-operator fun Time.plus(mins_in: Int): Time {
+operator fun Time.plus(mins_in: Int) : Time {
     val tot = this.abs + mins_in
     return Time(tot)
 }
 
-operator fun Time.plus(other: Time): Time {
+operator fun Time.plus(other: Time) : Time {
     val tot = this.abs + other.abs
     return Time(tot)
 }
 
 fun usage() {
+    println("""
+    |Usage: addtime [-qh] hh:mm mins_elapse
+    |Note: if mins_elapse is negative, precede it with '--'
+    |Options:
+    |        -q quietly output end time
+    |        -h print this message and exit
+    """.trimMargin())
     exitProcess(1)
 }
 
-fun fromString(str: String): Time {
+fun fromString(str: String) : Time {
     if (':' in str) {
         return Time(str)
     } else {
@@ -38,7 +47,6 @@ fun main(args: Array<String>) {
     val (opts, free) = args.partition{thing -> re.matches(thing)}
 
     if ("-h" in opts || free.size < 2) {
-        println("exit")
         usage()
     }
 
