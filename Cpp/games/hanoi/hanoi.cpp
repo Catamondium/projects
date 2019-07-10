e74fd08565513f609365ac126f9c2b763be7a87e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-07-10 14:05:11 +0100

    .

--- Cpp/games/hanoi/hanoi.cpp
+++ Cpp/games/hanoi/hanoi.cpp
@@ -6,7 +6,6 @@
 #include "iter_stack.hpp"
 #include "../game.hpp"
 using Tower = Iter_stack<int>;
-//using namespace std;
 
 std::string printTower(Tower t)
 // Printed representation for cmdline
@@ -45,7 +44,7 @@ std::string /* Error string */ transfer(std::array<Tower, N> &ts, int from, int 
 
     if (mover && reciever && mover > reciever)
     {
-        return "Illegal move: " + to_string(mover.value_or(0)) + " > " + to_string(reciever.value_or(0));
+        return "Illegal move: " + to_string(mover.value(0)) + " > " + to_string(reciever.value(0));
     }
 
     if (mover)
