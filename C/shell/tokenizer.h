f420dae4a79a132f796b8db1f65f7748a0aad62e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-07-15 22:24:59 +0100

    .

--- C/shell/tokenizer.h
+++ C/shell/tokenizer.h
@@ -60,7 +60,43 @@ void _tok_addnulls(char *line)
 char **tokenize(char *line)
 {
     size_t len = strlen(line);
-    _tok_addnulls(line);
-    char **ptrs = _tok_makeptrs(line, len);
+    bool inquote = false;
+    char **ptrs = malloc(sizeof(char *) * len);
+    size_t position = 0;
+    bool inreal = false;
+
+    for (int i = 0; i < len; ++i)
+    {
+        if (line[i] == '\\') // skip escaped
+        {
+            if (!inreal)
+            {
+                inreal = true;
+                ptrs[position++] = line + i;
+            }
+            ++i;
+        }
+        else if (line[i] == '\"') // entered real quote
+        {
+            inreal = false;
+            line[i] = 0;
+            inquote = !inquote;
+        }
+        else if (!inquote && isspace(line[i])) // entered real space
+        {
+            inreal = false;
+            line[i] = 0;
+        }
+        else
+        {
+            if (!inreal)
+            {
+                inreal = true;
+                ptrs[position++] = line + i;
+            }
+        }
+    }
+
+    ptrs[position] = NULL;
     return ptrs;
 }
