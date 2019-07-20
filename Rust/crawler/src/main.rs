ea509594a3fb4efa865c3e0754403cc84118b451
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-07-21 00:02:26 +0100

    .

---
+++ Rust/crawler/src/main.rs
@@ -0,0 +1,79 @@
+extern crate rand;
+extern crate reqwest;
+
+use std::collections::HashSet;
+use std::error::Error;
+use std::vec::Vec;
+
+const POPULATION: usize = 100_000;
+const SAMPLERATIO: f64 = 0.1;
+const SAMPLE: usize = ((POPULATION as f64) * SAMPLERATIO) as usize;
+
+// TODO
+// // multithreading w/ http mutexed
+
+#[derive(PartialEq, Debug, Clone)]
+enum Msg {
+    Active,
+    Inactive,
+}
+
+/// Generate random set in range [0, max), of length n.
+/// panics if max < n.
+fn random_set(n: usize, max: usize) -> Vec<usize> {
+    if max < n {
+        panic!(format!(
+            "Insufficient range: [0, {}) to fill {} spots",
+            max, n
+        ))
+    }
+
+    let mut set: HashSet<usize> = HashSet::new();
+
+    while set.len() < n {
+        set.insert(rand::random());
+    }
+
+    // Not great, will do for statistical sampling,
+    // close enough to rand range w/o faff
+    return set.iter().map(|x| x % max).collect();
+}
+
+fn percent(x: usize, max: usize) -> usize {
+    return (100 * x) / max;
+}
+
+fn scale(x: usize, ratio: f64) -> usize {
+    return ((x as f64) / ratio) as usize;
+}
+
+fn crawl(id: usize) -> Result<Msg, Box<dyn Error>> {
+    let source = format!(
+        "https://www.example.co.uk/product_info.php?products_id={:05}",
+        id
+    );
+
+    let resp = reqwest::get(source.as_str())?.text()?;
+    if resp.contains("Product not found!") {
+        return Ok(Msg::Inactive);
+    } else {
+        return Ok(Msg::Active);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let set = random_set(10, 100);
+    let mut results = Vec::new();
+    println!("{:#?}", set);
+    for i in set {
+        results.push(crawl(i));
+    }
+    let filtered: Vec<Msg> = results.into_iter().filter_map(|e| e.ok()).collect();
+    println!(
+        "Active: {}\nInactive: {}",
+        filtered.iter().filter(|x| **x == Msg::Active).count(),
+        filtered.iter().filter(|x| **x == Msg::Inactive).count()
+    );
+
+    return Ok(());
+}
