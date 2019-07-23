2bd367d61fbd36e6b67907d65bd4fb7683cbd4bf
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-07-23 01:05:00 +0100

    .

--- Rust/gutenberg/src/main.rs
+++ Rust/gutenberg/src/main.rs
@@ -6,19 +6,26 @@ use std::fs::File;
 use std::hash::Hash;
 use std::io::{BufRead, BufReader};
 
+/* TODO
+ * extract titles from gutenberg headers
+ * multithread across multiple files
+ * * may require contrival for practice
+ * * time analysis?
+ */
+
 #[derive(Debug)]
-struct Counthash<K: Hash + Eq>(HashMap<K, usize>);
-impl<K: Hash + Eq> Counthash<K> {
+struct Counthash<K: Hash + Eq + Clone>(HashMap<K, usize>);
+impl<K: Hash + Eq + Clone> Counthash<K> {
     fn new() -> Self {
         Counthash(HashMap::new())
     }
 
-    fn add(&mut self, key: K) {
+    fn add(&mut self, key: &K) {
         let item = self.0.get_mut(&key);
         if let Some(count) = item {
             *count += 1;
         } else {
-            self.0.insert(key, 1);
+            self.0.insert(key.clone(), 1);
         }
     }
 
@@ -29,7 +36,8 @@ impl<K: Hash + Eq> Counthash<K> {
 
 #[derive(Debug)]
 struct Stats {
-    counts: Counthash<String>,
+    count: usize,
+    mode: Option<(String, usize)>,
     longest: Option<String>,
     shortest: Option<String>,
 }
@@ -37,14 +45,20 @@ struct Stats {
 impl Stats {
     fn new() -> Self {
         Stats {
-            counts: Counthash::new(),
+            count: 0,
+            mode: None,
             longest: None,
             shortest: None,
         }
     }
 }
 
+fn unmut<A: Clone, B: Clone>(opt: &Option<(&A, &B)>) -> Option<(A, B)> {
+    opt.map(|(a, b)| (a.clone(), b.clone()))
+}
+
 fn analyze(source: BufReader<File>) -> Stats {
+    // Should we just use the hash to derive other stats?
     let mut words: Counthash<String> = Counthash::new();
     let mut longest: Option<String> = None;
     let mut shortest: Option<String> = None;
@@ -57,7 +71,7 @@ fn analyze(source: BufReader<File>) -> Stats {
         }
 
         for word in uline.split(char::is_whitespace).filter(|w| *w != "") {
-            words.add(word.clone().to_owned());
+            words.add(&word.to_string());
             longest = longest
                 .filter(|v| v.len() > word.len())
                 .or(Some(word.to_string()));
@@ -68,8 +82,13 @@ fn analyze(source: BufReader<File>) -> Stats {
         }
     }
 
+    let mode = words.0.iter().fold(None, |acc, (w, c)| {
+        acc.filter(|(_, ac)| *ac >= c).or(Some((w, c)))
+    });
+
     Stats {
-        counts: words,
+        count: words.sum(),
+        mode: unmut(&mode),
         longest,
         shortest,
     }
@@ -81,16 +100,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let reader = BufReader::new(handler);
 
     let stats = analyze(reader);
-    let mode = stats.counts.0.iter().fold(None, |acc, (w, c)| {
-        acc.filter(|(_, ac)| *ac >= c).or(Some((w, c)))
-    });
 
     println!(
         "count: {}\nmode: {:?}\nlongest: {:?}\nshortest: {:?}",
-        stats.counts.sum(),
-        mode,
-        stats.longest,
-        stats.shortest
+        stats.count, stats.mode, stats.longest, stats.shortest
     );
 
     Ok(())
