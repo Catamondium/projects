c0e098d340407f66ccd8e79d42bd3d216cdf029c
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-08-09 00:19:42 +0100

    removed redundant directories

--- Python/socket/client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import socket
-
-clidata = socket.gethostname(), 5007
-
-with socket.socket() as client:
-    client.connect(clidata)
-    msg = client.recv(24)
-print(f"Recieved:\t{msg.decode()}", end='')
