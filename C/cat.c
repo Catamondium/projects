d97f97d76cd9af612bc5ea5be691f21ac8a82ddf
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-08-21 16:59:57 +0100

    inverted stdin detection semantics

--- C/cat.c
+++ C/cat.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define handle_error(msg)   \
-    do                      \
-    {                       \
-        perror(msg);        \
-        putchar('\n');      \
-        exit(EXIT_FAILURE); \
-    } while (0)
-
 void cat(FILE *f)
 {
     for (int ch = fgetc(f); ch != EOF; ch = fgetc(f))
@@ -28,11 +20,11 @@ int main(int argc, char **argv)
     { // read spec in argv
         for (int i = 1; i < argc; ++i)
         {
-            int readstd = strcmp(argv[i], "-") == 0;
-            FILE *file = (readstd) ? stdin : fopen(argv[i], "r");
+            int readfile = strcmp(argv[i], "-") != 0;
+            FILE *file = (readfile) ? fopen(argv[i], "r") : stdin;
             cat(file);
 
-            if (!readstd)
+            if (readfile)
             {
                 fclose(file);
             }
