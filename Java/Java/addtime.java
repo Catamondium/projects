3a834148e5a34257df8ecf4e7ffa858d9bb163f6
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-08-23 12:54:09 +0100

    .

--- Java/Java/addtime.java
+++ Java/Java/addtime.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
+import java.util.regex.*;
+import java.util.Vector;
 
 class Time {
-    public int hrs;
-    public int mins;
+    public int hrs = 0;
+    public int mins = 0;
 
     public Time(String strTime) {
-        String[] parts = strTime.split(":");
-        this.hrs = Integer.parseInt(parts[0]);
-        this.mins = Integer.parseInt(parts[1]);
+        if (strTime.contains(":")) {
+            String[] parts = strTime.split(":");
+            this.hrs = Integer.parseInt(parts[0]);
+            this.mins = Integer.parseInt(parts[1]);
+        } else {
+            int mins = Integer.parseInt(strTime);
+            this.hrs = Math.floorDiv(mins, 60);
+            this.mins = Math.floorMod(mins, 60);
+        }
     }
 
     public Time(int hrs, int mins) {
@@ -15,37 +23,80 @@ class Time {
         this.mins = mins;
     }
 
-    public static Time elapse(Time s, int e) {
-        int offset = (s.hrs * 60) + s.mins;
-        int tot = offset + e;
-        return new Time((int) Math.floor(tot / 60), tot % 60);
+    public Time(int mins) {
+        this.hrs = Math.floorDiv(mins, 60);
+        this.mins = Math.floorMod(mins, 60);
     }
 
-    public String format() {
+    public Time elapse(int e) {
+        return new Time(this.abs() + e);
+    }
+
+    public Time elapse(Time other) {
+        return new Time(this.abs() + other.abs());
+    }
+
+    public int abs() {
+        return (this.hrs * 60) + this.mins;
+    }
+
+    public String toString() {
         return String.format("%02d:%02d", this.hrs, this.mins);
+    }
+}
+
+class Argparse {
+    public static Pattern optRe = Pattern.compile("^-[-a-zA-Z]+");
+    public Vector<String> opts = new Vector<String>();
+    public Vector<String> free = new Vector<String>();
+
+    Argparse(String[] argv) {
+        for (String s : argv) {
+            Matcher m = optRe.matcher(s);
+            if (m.matches()) {
+                this.opts.add(s);
+            } else {
+                this.free.add(s);
+            }
+        }
+    }
+
+    public boolean hasOpt(String opt) {
+        return (opts.indexOf(opt) != -1);
     }
 }
 
 class addtime {
     public static void usage() {
-        System.out.println("Usage: <HH:MM> <mins>");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Usage: [-hql] <HH:MM> <mins | HH:MM>\n");
+        sb.append("options:\n\t-q quietly output end time\n");
+        sb.append("\t-h print this message and exit\n");
+        sb.append("\t-l print written language");
+        System.out.println(sb);
         System.exit(1);
     }
 
     public static void main(String[] argv) {
-        boolean version_opt = Arrays.stream(argv).anyMatch(x -> x.equals("-l"));
-        if (argv.length < 2)
+        Argparse args = new Argparse(argv);
+
+        if (args.hasOpt("-h")) {
             usage();
-        else if (version_opt) {
+        } else if (args.hasOpt("-l")) {
             System.out.format("Java: %s\n", System.getProperty("java.version"));
             System.exit(0);
+        } else if (args.free.size() < 2)
+            usage();
+
+        Time start = new Time(args.free.get(0));
+        Time elapse = new Time(args.free.get(1));
+        Time end = start.elapse(elapse);
+
+        if (args.hasOpt("-q")) {
+            System.out.println(end);
+        } else {
+            System.out.format("Start:\t%s\t%+d\n", start, elapse.abs());
+            System.out.format("End:\t%s\n", end);
         }
-
-        Time start = new Time(argv[0]);
-        int elapse = Integer.parseInt(argv[1]);
-        Time end = Time.elapse(start, elapse);
-
-        System.out.format("Start:\t%s\t%+d\n", start.format(), elapse);
-        System.out.format("End:\t%s\n", end.format());
     }
 }
