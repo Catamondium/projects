395ec7d0c2d81403d083e1706a9e5ccadbc8d242
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-10-01 23:55:50 +0100

    Developed form & linked date loader up

--- Python/calbot/gui.py
+++ Python/calbot/gui.py
@@ -3,15 +3,16 @@ import PySimpleGUI as sg
 from parser import parse, GB_FORMAT, tclsmap, Holiday
 from sys import argv
 from datetime import datetime as dt
+from itertools import starmap
 
 
 """
-Design idea:
+Design idea: TODO
     Box sizes = (10, 1) for dates
 
-    Enumerate holidays in Table
-    Spins & Radios selects element to edit
-    Buttons for Append, Remove, Edit & Submit
+    #Enumerate holidays in Table
+    #Spins & Radios selects element to edit
+    #Buttons for Append, Remove, Edit & Submit
 
     Load selected bound into In, Cal pair? In requires validation
 
@@ -25,51 +26,80 @@ def gb_conv(value):
     return dt.strftime(value, GB_FORMAT)
 
 
-def tabulate(dvals):
+def tabulate(dvals, **kwargs):
     """
     Tabulate window values dates
     """
-    starts = sorted(datum for datum in dvals if 's' in datum)
-    ends = sorted(datum for datum in dvals if 'e' in datum)
-    table_v = [[dvals[s], dvals[e]] for s, e in zip(starts, ends)]
-    return sg.Table(table_v, key='holidays', headings=['START', 'END'], justification='center')
+    table_v = list(map(list, dvals))
+    return sg.Table(table_v, headings=['START', 'END'], **kwargs)
 
 
-def radio_group(title, choices, group_id, frame=True):
+def radio_group(title, choices, group_id, frame=False, sep='', **kwargs):
     """
     Generate framed horisontal Radio group
     keyed in {title}_{choice} form
     """
-    radios = [sg.Radio(ch, group_id, key=f"{title}_{ch}") for ch in choices]
+    radios = [sg.Radio(ch, group_id, key=f"{title}{sep}{ch}", **kwargs) for ch in choices]
     return sg.Frame(title, [radios]) if frame else radios
 
 
-def gen(*hols):
-    width = len(str(len(hols)))
-    return [[sg.In(h.start, key=f"hol_{i:0{width}}s", size=(10, 1)), sg.CalendarButton('cal', target=(i, 0), format=GB_FORMAT),
-            sg.In(h.end, key=f"hol_{i:0{width}}e", size=(10, 1)), sg.CalendarButton('cal', target=(i, 2), format=GB_FORMAT)] for i,h in enumerate(hols)]
-
 if __name__ == "__main__":
+    sg.change_look_and_feel('GreenTan')
     if len(argv) > 1:
         descriptor = argv[1]
     else:
-        descriptor = sg.PopupGetFile('Open calendar descriptor')
+        descriptor = sg.PopupGetFile('Open calendar descriptor', file_types=(("ALL FILES", "*"),))
+
+    if descriptor is None:
+        exit(0)
+
     with open(descriptor) as f:
-        data = tclsmap(Holiday, gb_conv, parse(f))
+        data = list(tclsmap(Holiday, gb_conv, parse(f)))
 
-    layout = [*gen(*data), [sg.Quit()]]
-    w = sg.Window('Cal manager', layout)
-    event, values = w.read()
-    w.close()
+    try:
+        init = data[0] and 0
+    except KeyError:
+        init = None
+    ctl = [
+            [
+                sg.In(key='-D_TXT-', size=(10, 1), justification='center'),
+                sg.CalendarButton('Cal', target='-D_TXT-', format=GB_FORMAT, key='-D_RAW-')
+            ],
+            [
+                *radio_group('', ['-START-', '-END-'], 0, enable_events=1),
+                sg.Spin(list(range(len(data))), initial_value=init, key='-ROW-', enable_events=1)
+            ],
+            [
+                sg.Button('Add'), sg.Button('Remove'),
+                sg.Button('Apply')]
+        ]
 
-    layout = [[*radio_group('Bound', ['Start', 'End'], 0, False), sg.Button('Append'), sg.Spin(list(range(len(values) // 2)))], [tabulate(values)], [sg.Quit()]]
-    w = sg.Window('Values', layout).finalize()
-    hols = w['holidays']
+    layout = [
+                [
+                    tabulate(data, key='-DATA-', justification='center'),
+                    sg.Frame('Controls', ctl, element_justification='center')
+                ],
+                [
+                    sg.Quit()
+                ]
+        ]
+    w = sg.Window('Main', layout).finalize()
+    hols = w['-DATA-']
+    w['-START-'].update(True)
+    fields = ('-START-', '-END-')
     while True:
         event, values = w.read()
         if event in (None, 'Quit'):
             break
-        else:
-            hols.update(values=hols.get() + [['A', 'B']]) # Table is updateable
-            print(values)
+        elif event == 'Append':
+            hols.update(values=hols.get() + [['A', 'B']])
+        elif event in ('-ROW-',) + fields:
+            row = int(w['-ROW-'].get() or -1)
+            field = 0 if w['-START-'].get() else 1
+            if row == -1:
+                continue
+            val = w['-DATA-'].get()[row][field]
+            w['-D_TXT-'].update(val)
+            print(f"Event: {event}")
+            print(f"Vals: {values}")
     w.close()
