ec1304ca596d5c4e1d18add2f7be993b46d3c468
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-11-02 13:21:39 +0000

    linting

--- Cpp/games/roam/main.cpp
+++ Cpp/games/roam/main.cpp
@@ -18,33 +18,37 @@ for implementing a Lua moddable application
 
 #define LUA_ROAM "internal.lua"
 
+struct Player {
+    Vec pos;
+};
+
 struct RoamGame:public CursesGame < 2 > {
-    Vec fruit;
+    Player player; // pass to lua via userdata closure?
     lua_State *L;
 
-     RoamGame() {
-	pallete = {
-	    Color {
-	    FOOD, COLOR_GREEN, COLOR_BLACK}, Color {
-	PLAYER, COLOR_RED, COLOR_BLACK},};
-	L = luaL_newstate();
-	assert(L != NULL);
-	luaL_openlibs(L);
-	assert(LUA_OK == luaL_dofile(L, LUA_ROAM));
+    RoamGame() {
+        pallete = {
+            Color {FOOD, COLOR_GREEN, COLOR_BLACK},
+            Color {PLAYER, COLOR_RED, COLOR_BLACK},
+        };
+        L = luaL_newstate();
+        assert(L != NULL);
+        luaL_openlibs(L);
+        assert(LUA_OK == luaL_dofile(L, LUA_ROAM));
     }
 
     ~RoamGame() {
-	lua_close(L);
+	    lua_close(L);
     }
 
     void init() override {
-	setFrameRate(10);
-	srand(time(NULL));
-	// mocked load procedure, mod locations undetermined
-	if (LUA_OK != luaL_dofile(L, "test.lua")) {
-	    std::cerr << "lua: " << lua_tostring(L, -1) << std::endl;
-	    throw "Lua exception";
-	}
+        setFrameRate(10);
+        srand(time(NULL));
+        // mocked load procedure, mod locations undetermined
+        if (LUA_OK != luaL_dofile(L, "test.lua")) {
+            std::cerr << "lua: " << lua_tostring(L, -1) << std::endl;
+            throw "Lua exception";
+        }
     }
 
     void loop() override;
