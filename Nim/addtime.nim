2b5eaed0d9895e42e757751f928f7f875a72aae3
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-11-08 21:54:05 +0000

    addtime impl w/o argv support

---
+++ Nim/addtime.nim
@@ -0,0 +1,20 @@
+import strformat
+
+type
+  Minutes* = range[-59..59]
+  Time* = object
+    hrs*: int # top range, cannot have upper limit
+    mins*: Minutes
+
+proc abs(t: Time): int =
+  t.hrs*60 + t.mins
+ 
+proc `$`(t: Time): string =
+  fmt"{t.hrs}:{t.mins}"
+
+proc `+`(t: Time, o: Time): Time =
+  let tot = t.abs + o.abs
+  return Time(hrs: tot /% 60, mins: tot mod 60)
+   
+let t = Time(hrs: 11, mins: 30)
+echo t
