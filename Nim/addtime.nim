e2357c3ec065f3466c016ddbb874a9066c699cb2
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-11-08 21:54:56 +0000

    nimpretty

--- Nim/addtime.nim
+++ Nim/addtime.nim
@@ -8,13 +8,13 @@ type
 
 proc abs(t: Time): int =
   t.hrs*60 + t.mins
- 
+
 proc `$`(t: Time): string =
   fmt"{t.hrs}:{t.mins}"
 
 proc `+`(t: Time, o: Time): Time =
   let tot = t.abs + o.abs
   return Time(hrs: tot /% 60, mins: tot mod 60)
-   
+
 let t = Time(hrs: 11, mins: 30)
 echo t
