843708382c75c308be12811daa0f5b4bf409116b
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2019-12-22 15:34:19 +0000

    there, but not working; anomylous throws

--- Python/ASK/calc/calc.py
+++ Python/ASK/calc/calc.py
@@ -3,20 +3,29 @@ from ask_sdk_core.skill_builder import SkillBuilder
 
 from ask_sdk_core.utils import is_request_type, is_intent_name, get_slot_value
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.serialize import DefaultSerializer as DZ
 from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
 
-sb = SkillBuilder()
+from operator import add, mul
+from functools import reduce
 
-DEBUG = True
+sb = SkillBuilder()
 
 # NOTE, handler_input is GLOBAL; not needed in response
 
-def init_or_append(session, key, val):
-    if session[key] is None:
-        session[key] = [val]
-    else:
-        session[key].append(val)
+def product(stack):
+    return reduce(mul, stack, 1)
+def summate(stack):
+    return reduce(add, stack, 0)
+
+despatch = {
+    "add": summate,
+    "sum": summate,
+    "summate": summate,
+    "multiply": product,
+    "product": product
+}
 
 @sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
 def launch_request_handler(handler_input: HandlerInput):
@@ -41,32 +50,34 @@ def finish_intent_handler(handler_input: HandlerInput):
 @sb.request_handler(can_handle_func=is_intent_name("OperandIntent"))
 def operand_intent_handler(handler_input: HandlerInput):
     val = float(get_slot_value(handler_input, "operand"))
-    print(f"STACK {val}")
-    init_or_append(handler_input.attributes_manager.session_attributes, "stack", val)
-    text = "Operand intent, placeholder"
+    stack = []
+    try:
+        stack = DZ.deserialize(handler_input.attributes_manager.session_attributes['stack'], 'List[float]')
+    except:
+        pass
+    stack.append(val)
+    handler_input.attributes_manager.session_attributes['stack'] = DZ.serialize(stack)
+    text = f"Operand intent, {val}"
 
     handler_input.response_builder.speak(text).set_card(
-        SimpleCard("Calculator", text)).set_should_end_session(DEBUG)
+        SimpleCard("Calculator", text)).set_should_end_session(False)
     return handler_input.response_builder.response
 
 @sb.request_handler(can_handle_func=is_intent_name("OperatorIntent"))
 def operator_intent_handler(handler_input: HandlerInput):
     op = get_slot_value(handler_input, "operand")
-    # What does op display as "value the user spoke"?
-    print(f"OPERATOR \'{op}\'")
+    stack = DZ.deserialize(handler_input.attributes_manager.session_attributes["stack"], 'list[float]')
+    handler_input.attributes_manager.session_attributes["stack"] = DZ.serialize([despatch[op](stack)])
 
-    # TODO dict despatch reduction functions
-    # TODO clear session attributes, push return
-
-    text = "Operator intent, placeholder"
+    text = f"Operator intent, {op}"
     handler_input.response_builder.speak(text).set_card(
-        SimpleCard("Calculator", text)).set_should_end_session(DEBUG)
+        SimpleCard("Calculator", text)).set_should_end_session(False)
     return handler_input.response_builder.response
 
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
 def help_intent_handler(handler_input: HandlerInput):
     text = "Stack-based calculator"
-    handler_input.response_builder.speak(text).ask(text).set_card(
+    handler_input.response_builder.speak(text).set_card(
         SimpleCard("Calculator", text)).set_should_end_session(False)
     return handler_input.response_builder.response
 
@@ -99,4 +110,4 @@ def all_exception_handler(handler_input: HandlerInput, exception):
     handler_input.response_builder.speak(speech).ask(speech)
     return handler_input.response_builder.response
 
-handler = sb.lambda_handler()
+lambda_handler = sb.lambda_handler()
