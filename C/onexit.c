e010a1c1273cdaeaf19374d542454eed3f2e4d68
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2019-12-26 17:50:16 +0000

    now passing arguments through

--- C/onexit.c
+++ C/onexit.c
@@ -2,31 +2,47 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <signal.h>
+#include <assert.h>
 #include <stdio.h>
 
 void usage(char *prog, int status)
 {
-    printf("Usage: %s [-h] PID COMMAND\n"
-	   "PID: process ID to monitor\n"
-	   "COMMAND: command on exit\n"
-	   "-h: display this usage message\n", prog);
+    printf("Usage: %s [-h] PID COMMAND [ARGS]...\n"
+	   "\tPID: process ID to monitor\n"
+	   "\tCOMMAND: command on exit\n"
+	   "\tARGS: command arguments\n"
+	   "\t-h: display this usage message\n", prog);
     exit(status);
 }
 
-void onexit(pid_t pid, char *com)
+void onexit(pid_t pid, char **com)
 {
-    while (kill(pid, 0) != -1) {
+    while (kill(pid, 0) != -1)
 	sleep(5);
-    }
 
-    if (system(com) == -1) {
-	perror("system");
+    if (execvp(com[0], com) == -1)
+	perror("trigger");
+}
+
+char **build_com(int argc, int fromn, char **argv)
+{
+    int n = 0;
+    char **com = malloc(sizeof(char *) * (argc - fromn + 1));
+
+    for (int i = fromn; i < argc; ++i) {
+	com[n] = argv[i];
+    ++n;
     }
+    com[argc - fromn] = NULL;
+
+    return com;
 }
 
 int main(int argc, char **argv)
 {
     int opt;
+    char **com;
+
     while ((opt = getopt(argc, argv, "h")) != -1) {
 	if (opt == 'h')
 	    usage(argv[0], EXIT_SUCCESS);
@@ -37,5 +53,8 @@ int main(int argc, char **argv)
 	usage(argv[0], EXIT_FAILURE);
     }
 
-    onexit(atoi(argv[optind]), argv[optind + 1]);
+    com = build_com(argc, optind + 1, argv);
+    assert(com[0] != NULL);
+    onexit(atoi(argv[optind]), com);
+    free(com);
 }
