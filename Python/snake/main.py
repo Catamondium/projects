95325efa1e89160132756c669ab1d49ebd157b69
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2020-07-23 09:30:10 +0100

    snake clone

---
+++ Python/snake/main.py
@@ -0,0 +1,57 @@
+import sys, pygame
+from player import Vec, Snake
+pygame.init()
+
+SIZE = 600, 600
+GRID = width, height = 30, 30
+black = 0, 0, 0
+white = 255, 255, 255
+
+screen = pygame.display.set_mode(SIZE)
+
+SLEN = 5
+FRATE = 15
+snake = Snake(GRID, SLEN)
+food = Vec.random(*GRID)
+
+run = False
+clk = pygame.time.Clock()
+pygame.font.init()
+font = pygame.font.Font(pygame.font.get_default_font(), 18)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_q]:
+                sys.exit()
+            elif pressed[pygame.K_a] or pressed[pygame.K_LEFT]:
+                snake.dir(-1, 0)
+            elif pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
+                snake.dir(1, 0)
+            elif pressed[pygame.K_w] or pressed[pygame.K_UP]:
+                snake.dir(0, -1)
+            elif pressed[pygame.K_s] or pressed[pygame.K_DOWN]:
+                snake.dir(0, 1)
+
+    clk.tick_busy_loop(FRATE)
+    screen.fill(black)
+
+    state = snake.update(GRID, food)
+
+    scr = f"SCORED: {len(snake) - SLEN}"
+    if state == Snake.State.DEAD:
+        print(scr)
+        snake = Snake(GRID, SLEN)
+    elif state == Snake.State.EATEN:
+        food = Vec.random(*GRID)
+
+    snake.render(screen, GRID)
+    print(snake)
+    food.render(screen, GRID)
+
+    text_surface = font.render(scr, True, white)
+    screen.blit(text_surface, dest=(0,0))
+    pygame.display.flip()
+    run = True
---
+++ Python/snake/player.py
@@ -0,0 +1,113 @@
+import pygame
+from collections import deque
+from enum import Enum, auto
+from random import randint
+from math import floor
+
+class Vec:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def random(maxx, maxy):
+        return Vec(randint(0, maxx), randint(0, maxy))
+
+    @property
+    def i(self):
+        return self.x
+    
+    @property
+    def j(self):
+        return self.j
+
+    def render(self, screen, grid, color=(0, 255, 0)):
+        gw, gh = grid
+        w, h = screen.get_size()
+
+        # cell size
+        c_w = floor(w / gw)
+        c_h = floor(h / gh)
+        
+        rect = pygame.Rect(self.x * c_w, self.y * c_h, c_w, c_h)
+        pygame.draw.rect(screen, color, rect)
+
+    def __add__(self, other):
+        return Vec(self.x + other.x, self.y + other.y)
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __mod__(self, other):
+        return Vec(self.x % other.x, self.y % other.y)
+    
+    def __neg__(self):
+        return Vec(-self.x, -self.y)
+
+    def __repr__(self):
+        return f"<{self.x}, {self.y}>"
+
+class Snake:
+    class State(Enum):
+        EATEN = auto()
+        ALIVE = auto()
+        DEAD = auto()
+
+    def __init__(self, grid, length):
+        width, height = grid
+        print(Vec.random(width - length, height))
+        body = deque()
+
+        origin = Vec.random(width - length, height)
+
+        for i in range(length):
+            body.append(origin + Vec(i, 0))
+        self.body = body
+        self.vel = Vec()
+        self.state = Snake.State.ALIVE
+
+    def update(self, grid, food):
+        head = self.body[0]
+        self.state = self.State.ALIVE
+
+        if head == food:
+            self.state = self.State.EATEN
+            nhead = head + self.vel
+            self.body.appendleft(nhead)
+            head = self.body[0]
+
+        if self.vel != Vec():
+            head += self.vel
+            self.body.pop()
+            self.body.appendleft(head)
+        
+        width, height = grid
+        # self collision
+        if self.died(width, height):
+            self.state = self.State.DEAD
+        
+        return self.state
+
+    def render(self, screen, grid):
+        d = floor(255 / len(self.body))
+        for i, v in enumerate(self.body):
+            v.render(screen, grid, (255 - d * i, 0, 255))
+
+    def died(self, width, height):
+        return self.body[0] in list(self.body)[1:] or self.oob(width, height)
+    
+    def oob(self, width, height):
+        head = self.body[0]
+        return head.x < 0 or head.x > width or\
+            head.y < 0 or head.y > height
+
+    def dir(self, dx, dy):
+        dv = Vec(dx, dy)
+        if self.vel != -dv: # No reversing
+            self.vel = dv
+
+    def __repr__(self):
+        return f"Snake{self.body[0], self.vel}"
+    
+    def __len__(self):
+        return len(self.body)
