b1810fb62e1b8d9e84967db7b86de5e565b6d0e2
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2021-07-09 07:54:22 +0100

    downloading capability

--- Python/updir/updir.py
+++ Python/updir/updir.py
@@ -65,6 +65,17 @@ def large_upload(dbx, f, file_size, dest_path):
                 f.read(CHUNK_SIZE), cursor.session_id, cursor.offset)
             cursor.offset = f.tell()
 
+def dbx_list(dbx, drop):
+    files = []
+    resp = dbx.files_list_folder(drop, recursive=True, include_non_downloadable_files=False)
+    files += [x.path_display for x in resp.entries
+                if isinstance(x, dropbox.files.FileMetadata)]
+    while resp.has_more:
+        resp = dbx.files_list_folder_continue(resp.cursor)
+        files += [x.path_display for x in resp.entries
+                    if isinstance(x, dropbox.files.FileMetadata)]
+    return set(files)
+
 def small_upload(dbx, f, file_size, dest_path):
     dbx.files_upload(f.read(), dest_path,
                     mode=dropbox.files.WriteMode("overwrite"))
@@ -77,33 +88,56 @@ def upload(dbx, f, file_size, dest_path):
         large_upload(
             dbx, f, file_size, dest_path)
 
+def download(dbx, dnames, local, drop):
+    for drop_path in tqdm(dnames, desc="Down"):
+        relative_path = Path(drop_path).relative_to(drop)
+        local_path = Path(local) / relative_path
+        local_path.parent.mkdir(parents=True, exist_ok=True)
+        local_path.touch(exist_ok=True)
+        dbx.files_download_to_file(str(local_path), drop_path)
+
 def _main():
     import argparse
-    parser = argparse.ArgumentParser("Upload directory to dropbox recursively")
+    parser = argparse.ArgumentParser("Manage dropbox stuff")
     parser.add_argument("pair", type=pair, nargs='+',
                         help="LOCAL,DROP directory pairs")
-    parser.add_argument("--dry", "-d", action='store_true',
+    parser.add_argument("--dry", action='store_true',
                         help="Print changes w/o sending")
+
+    parser.add_argument("--down", "-d", action='store_true',
+                        help="Download recursively")
+    parser.add_argument("--up", "-u", action='store_true',
+                        help="Upload recursively")
+    parser.add_argument("--sync", "-s", action='store_true',
+                        help="Print upload, then download difference")
     args = parser.parse_args()
 
+    do_up = args.up or args.sync
+    do_down = args.down or args.sync
     client = connect()
     for local, drop in args.pair:
         # enumerate local files recursively
-        length = sum(1 for x in Path(local).rglob("*"))
-        for local_path in tqdm(Path(local).rglob("*"), desc="Files done", total=length):
+        up = list(Path(local).rglob("*"))
+        ups = set()
+        if do_up:
+            for local_path in tqdm(up, desc="Up"):
 
-            if local_path.is_dir():
-                continue
+                if local_path.is_dir():
+                    continue
 
-            relative_path = local_path.relative_to(local)
-            drop_path = str(Path(drop) / relative_path)
-
-            if args.dry:
-                print(f"{local_path} -> {drop_path}")
-            else:
-                # upload the file
-                with open(local_path, 'rb') as f:
-                    upload(client, f, local_path.stat().st_size, drop_path)
+                relative_path = local_path.relative_to(local)
+                drop_path = str(Path(drop) / relative_path)
+                ups.add(drop_path)
+                if args.dry:
+                    print(f"{local_path} -> {drop_path}")
+                else:
+                    # upload the file
+                    with open(local_path, 'rb') as f:
+                        upload(client, f, local_path.stat().st_size, drop_path)
+        if do_down:
+            downs = dbx_list(client, drop)
+            diff = downs - ups
+            download(client, diff, local, drop)
 
 
 if __name__ == "__main__":
